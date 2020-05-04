1405e837b8c8d650281f69d40dbfd5afb04cde38
Patrick McGuire <insidenothing@gmail.com>
2020-05-04 11:30:53 -0400

    Update petition.php

--- petition.php
+++ petition.php
@@ -1,7 +1,7 @@
 <?PHP include_once('header.php');  ?>
 
   
- <div class='col-sm-12'> <img src='files/Green-Party-petition-1-page-001.jpg'> </div>
+ <div class='col-sm-12'> <img class="img-responsive" src='files/Green-Party-petition-1-page-001.jpg'> </div>
   
   
   
