1fb7899f50526f58b7922fa7f25e1798fd316638
Patrick McGuire <insidenothing@gmail.com>
2020-05-04 11:28:08 -0400

    Create petition.php

---
+++ petition.php
@@ -0,0 +1,16 @@
+<?PHP include_once('header.php');  ?>
+
+  
+ <div class='col-sm-12'> <img src='files/Green-Party-petition-1-page-001.jpg'> </div>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+<?PHP include_once('footer.php');
