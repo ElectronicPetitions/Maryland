42af63a39bf34961fcb79b16db71e9f84f9f7a8d
Patrick McGuire <insidenothing@gmail.com>
2020-05-04 10:33:07 -0400

    Create intro.php

---
+++ intro.php
@@ -0,0 +1,19 @@
+<?PHP include_once('header.php');  ?>
+
+<div class="container">
+  
+ <div class='col-sm-12'> are you a registeded maryland voter </div>
+  
+  <div class='col-sm-6'>yes </div>
+  
+  <div class='col-sm-6'>no </div>
+  
+  
+  
+  
+  
+  
+  
+</div>
+
+<?PHP include_once('footer.php');
