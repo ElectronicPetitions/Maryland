b7349fe73f326e9f27412d951203d5f55b1878db
Patrick McGuire <insidenothing@gmail.com>
2020-05-04 11:17:50 -0400

    Create is_the_information_correct.php

---
+++ is_the_information_correct.php
@@ -0,0 +1,31 @@
+<?PHP include_once('header.php');  ?>
+
+  
+ <div class='col-sm-12'>is this information correct </div>
+  
+  <div class='col-sm-6'>First Name </div><div class='col-sm-6'>Bob</div>
+  
+  <div class='col-sm-6'>Middle Name </div><div class='col-sm-6'>Edward</div>
+  
+  <div class='col-sm-6'>Last Name </div><div class='col-sm-6'>Smith</div>
+  
+  <div class='col-sm-6'>Full Addresss </div><div class='col-sm-6'>1232 Roadly Ct.</div>
+  
+  <div class='col-sm-2'>City</div><div class='col-sm-2'>Baltimore</div>
+  <div class='col-sm-2'>County</div><div class='col-sm-2'>Baltimore City</div>
+  <div class='col-sm-2'>Zip</div><div class='col-sm-2'>21204</div>
+
+  
+  <div class='col-sm-6'><button type="button" class="btn btn-success">YES</button> </div>
+  
+  <div class='col-sm-6'><button type="button" class="btn btn-danger">NO</button>  </div>
+  
+  
+  
+  
+  
+  
+  
+
+
+<?PHP include_once('footer.php');
