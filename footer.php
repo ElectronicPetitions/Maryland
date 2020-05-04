7d75ff565881ad40aaeac503f2382587eae06f2c
Patrick McGuire <insidenothing@gmail.com>
2020-05-04 11:06:16 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div class='col-sm-12'><?PHP echo $_SERVER['SCRIPT_NAME'];?> &COPY 2020 Patrick McGuire </div>
+<div class='col-sm-12'><?PHP echo $_SERVER['SCRIPT_NAME'];?> &COPY 2020 <a href='https://github.com/insidenothing/MarylandElectronicPetitionSignature'>Open Source at GitHub</a></div>
 
 
 </div>
