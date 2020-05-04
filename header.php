d8e0d9df3e9eb4775c564715f114280480da4e40
Patrick McGuire <insidenothing@gmail.com>
2020-05-04 11:29:07 -0400

    Update header.php

--- header.php
+++ header.php
@@ -6,11 +6,13 @@
 <div class="container"><!--- Open Container -->
   
 <ul class="nav nav-pills">
+
   <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/intro.php'){ echo "class='active'"; } ?> ><a href="intro.php">Intro</a></li>
   <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/not_a_registered_voter.php'){ echo "class='active'"; } ?> ><a href="not_a_registered_voter.php">Not a Registered Voter</a></li>
   <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/enter_information.php'){ echo "class='active'"; } ?> ><a href="enter_information.php">Enter Information</a></li>
   <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/is_the_information_correct.php'){ echo "class='active'"; } ?> ><a href="is_the_information_correct.php">Is The Information Correct</a></li>
 
+  <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/petition.php'){ echo "class='active'"; } ?> ><a href="petition.php">Petition</a></li>
   
   
   
