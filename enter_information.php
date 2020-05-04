09a581dc26982ae1dbafa3750a128de97a0da0cc
Patrick McGuire <insidenothing@gmail.com>
2020-05-04 11:09:17 -0400

    Create enter_information.php

---
+++ enter_information.php
@@ -0,0 +1,27 @@
+<?PHP include_once('header.php');  ?>
+
+  
+ <div class='col-sm-12'> please enter your name and zip code as it appears on your Maryland Voter Registration </div>
+  
+  <div class='col-sm-6'>first name </div>
+  
+  <div class='col-sm-6'>last name </div>
+  
+  
+    <div class='col-sm-6'><input> </div>
+  
+  <div class='col-sm-6'><input> </div>
+  
+  <div class='col-sm-6'>house number</div>
+  
+  <div class='col-sm-6'>zip code </div>
+  
+  
+    <div class='col-sm-6'><input> </div>
+  
+  <div class='col-sm-6'><input> </div>
+  
+  <div class='col-sm-12'> <input type='submit'></div>
+
+
+<?PHP include_once('footer.php');
