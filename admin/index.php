756271d69432b5e02f7b3ded2e0508765b158b16
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 08:41:25 -0400

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,20 @@
-admin home
+<?PHP 
 
+include_once('security.php');
+
+include_once('header.php');
+
+?>
+
+<h1>admin home</h1>
 
 
 
 <pre>
 <?PHP print_r($_COOKIE); ?>
 </pre>
+
+
+<?PHP
+include_once('footer.php');
+?>
