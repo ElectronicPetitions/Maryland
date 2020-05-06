f53eb1db694b99c07d581b5adcb7858a2b90db8b
Patrick McGuire <insidenothing@gmail.com>
2020-05-05 23:45:40 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-  <div class='col-sm-12' style='text-align:center;'>&COPY 2020 <a href='https://github.com/insidenothing/MarylandElectronicPetitionSignature'>Open Source at GitHub</a></div>
+  <div class='col-sm-12' style='text-align:center;'>&COPY; 2020 <a href='https://github.com/insidenothing/MarylandElectronicPetitionSignature'>Open Source at GitHub</a></div>
   <div class='col-sm-12' style='text-align:center;'><small><?PHP echo $_SERVER['SCRIPT_NAME'];?></small></div>
   </div>
 </body>
