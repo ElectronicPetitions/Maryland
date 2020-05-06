7e34637f97d4658931765c58a68a88e73605b8e1
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 13:26:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
   <div class='col-sm-12' style='height:50px; text-align:center;'> Are you a Registeded Maryland Voter?</div>
   
-  <div class='col-sm-6' style='height:50px; text-align:center;'><button type="button" class="btn btn-success">YES</button></div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><button type="button" class="btn btn-success" onclick="window.location.href='enter_information.php'>YES</button></div>
   
-  <div class='col-sm-6' style='height:50px; text-align:center;'><button type="button" class="btn btn-danger">NO</button> </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><button type="button" class="btn btn-danger" onclick="window.location.href='not_a_registered_voter.php'">NO</button> </div>
   
 <?PHP include_once('footer.php');
