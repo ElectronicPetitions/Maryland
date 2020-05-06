62c5da459335d11a30611e02cc26ac696c9bf4b5
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 08:56:50 -0400

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -4,5 +4,15 @@ foreach ( $_COOKIE as $key => $value ){
 	unset($_COOKIE[$key]);
 	setcookie($key, '', time() - 3600, '/'); 
 }
+// unset cookies
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
 header('Location: index.php');
 ?>
