4f46b65ac7ac075cce7272328c4ecb856116a6d8
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 14:04:43 -0400

    Update enter_information.php

--- enter_information.php
+++ enter_information.php
@@ -1,6 +1,17 @@
-<?PHP include_once('header.php');  ?>
+<?PHP
+include_once('header.php');
 
-  <div class='col-sm-12' style='height:100px; text-align:center;'><h1>Please enter your Name and ZIP Code as it appears on your Maryland Voter Registration</h1></div>
+// if found send to is_the_information_correct.php
+
+// if not found send to warning_not_found.php
+
+// if missing data send to warning_incomplete.php
+
+?>
+
+<form method='POST'>
+  
+  <div class='col-sm-12' style='height:100px; text-align:center;'><h3>Please enter your Name and ZIP Code as it appears on your Maryland Voter Registration</h3></div>
   
   <div class='col-sm-6' style='height:50px; text-align:center;'><h3>First Name</h3></div>
   
@@ -19,5 +30,7 @@
   <div class='col-sm-6' style='height:50px; text-align:center;'><input> </div>
   
   <div class='col-sm-12' style='height:50px; text-align:center;'><button type="button" class="btn btn-success">Next</button></div>
+
+</form>
 
 <?PHP include_once('footer.php');
