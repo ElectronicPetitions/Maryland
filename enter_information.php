c364f2c3867d07226ce65ded0e2f0fdf8e96fa92
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 13:44:39 -0400

    Update enter_information.php

--- enter_information.php
+++ enter_information.php
@@ -2,22 +2,22 @@
 
   <div class='col-sm-12' style='height:100px; text-align:center;'><h1>Please enter your Name and ZIP Code as it appears on your Maryland Voter Registration</h1></div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><h3>First Name</h3></div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>First Name</h3></div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><h3>Last Name</h3></div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>Last Name</h3></div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><input> </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><input> </div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><input> </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><input> </div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><h3>House Number</h3></div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>House Number</h3></div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><h3>ZIP Code</h3></div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>ZIP Code</h3></div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><input> </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><input> </div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><input> </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><input> </div>
   
-  <div class='col-sm-12' style='height:100px; text-align:center;'><button type="button" class="btn btn-success">Next</button></div>
+  <div class='col-sm-12' style='height:50px; text-align:center;'><button type="button" class="btn btn-success">Next</button></div>
 
 <?PHP include_once('footer.php');
