110588ee2f6b2b55b7eb5cf0b09c97443fd5c536
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 01:15:21 -0400

    Update status.php

--- admin/status.php
+++ admin/status.php
@@ -1,13 +1,11 @@
 <?PHP include_once('../header.php');  ?>
-<meta http-equiv="refresh" content="1">
-  <h1>
+<meta http-equiv="refresh" content="5">
   <?PHP
   $expected = 4194252;
   if ($result = $petition->query("SELECT VTRID FROM VoterList")) {
     $row_cnt = $result->num_rows;
     $c = $row_cnt / $expected;
     $p = number_format($c,2)*100;
-    echo date('r').' '.number_format($row_cnt).' out of '.number_format($expected).' '.$p.'% done.';
-} ?>
-</h1>
-<?PHP include_once('../footer.php');  ?>
+    echo '<h1>'.date('r').'</h1><h1>'.number_format($row_cnt).' out of '.number_format($expected).' '.$p.'% done</h1>';
+} 
+include_once('../footer.php');  ?>
