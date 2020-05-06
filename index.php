8d253be65d9f25be5f5bebb1c1774d608786e632
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 13:17:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?PHP include_once('header.php');  ?>
-  <h3>Welcome!</h3>
-    Development In Progress -  Please Click Through the Menu to Review 
+
+  <div class='col-sm-12' style='height:50px; text-align:center;'> Are you a Registeded Maryland Voter?</div>
+  
+  <div class='col-sm-6' style='height:50px; text-align:center;'><button type="button" class="btn btn-success">YES</button></div>
+  
+  <div class='col-sm-6' style='height:50px; text-align:center;'><button type="button" class="btn btn-danger">NO</button> </div>
+  
 <?PHP include_once('footer.php');
