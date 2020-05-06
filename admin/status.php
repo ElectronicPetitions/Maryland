b911275ef2a4ff8971613c9e53295398a372d872
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 13:24:15 -0400

    Update status.php

--- admin/status.php
+++ admin/status.php
@@ -6,6 +6,6 @@
     $row_cnt = $result->num_rows;
     $c = $row_cnt / $expected;
     $p = number_format($c,2)*100;
-    echo '<h1>'.date('r').'</h1><h1>'.number_format($row_cnt).' out of '.number_format($expected).' '.$p.'% done</h1>';
+    echo '<h1>'.date('r').'</h1><h1>'.number_format($row_cnt).' out of '.number_format($expected).' '.$p.'% done</h1><title>'.$p.'% done</title>';
 } 
 include_once('../footer.php');  ?>
