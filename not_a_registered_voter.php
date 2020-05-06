23cc4f291ace2b027401f48bdea33f2d6453fa11
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 13:56:23 -0400

    Update not_a_registered_voter.php

--- not_a_registered_voter.php
+++ not_a_registered_voter.php
@@ -2,6 +2,8 @@
 
 <div class='col-sm-12' style='height:100px; text-align:center;'><h2>You must be a registered voter in the state of maryland to sign this petition.</h2></div>
 
-<div class='col-sm-12' style='height:100px; text-align:center;'>If you are eligible, you can <button type="button" class="btn btn-success" onclick="window.location.href='https://voterservices.elections.maryland.gov/OnlineVoterRegistration/InstructionsStep1'">Register Online</button></div>
- 
+<div class='col-sm-12' style='height:100px; text-align:center;'><h3>If you are eligible</h3></div>
+
+<div class='col-sm-12' style='height:100px; text-align:center;'>If you are eligible,  <button type="button" class="btn btn-success" onclick="window.location.href='https://voterservices.elections.maryland.gov/OnlineVoterRegistration/InstructionsStep1'">You Can Register Online Hhere</button></div>
+
 <?PHP include_once('footer.php');
