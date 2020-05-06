da07596249d43c0f3f0cce8b52dabcd701dd79a6
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 01:10:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?PHP include_once('header.php');  ?>
-<meta http-equiv="refresh" content="60">
   <h3>Welcome!</h3>
     Development In Progress -  Please Click Through the Menu to Review 
 <?PHP include_once('footer.php');
