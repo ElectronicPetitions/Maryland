e2bffda9546c61e49f0dacaec79d5742ef12911d
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 10:35:33 -0400

    Update eligible.php

--- eligible.php
+++ eligible.php
@@ -1,18 +1,24 @@
-<?PHP include_once('header.php');  ?>
+<?PHP include_once('header.php'); 
+
+// ok, do not list ones already signed
+
+
+
+?>
 
   
- <div class='col-sm-12'> are you also eligible to sign each of these petitions in baltimore city </div>
+ <div class='col-sm-12'> are you also eligible to sign each of these petitions [location name] </div>
   
-  <div class='col-sm-2'><input type='checkbox' checked> </div>
+  <div class='col-sm-2'><input type='checkbox'> </div>
   
   <div class='col-sm-10'>New Party: Maryland Green Party </div>
   
   
-  <div class='col-sm-2'><input type='checkbox' checked> </div>
+  <div class='col-sm-2'><input type='checkbox'> </div>
   
   <div class='col-sm-10'>New Party: Maryland Libertarian Party </div>
   
-  <div class='col-sm-2'><input type='checkbox' checked> </div>
+  <div class='col-sm-2'><input type='checkbox'> </div>
   
   <div class='col-sm-10'>Charter Amendment: Baltimore Transit Equity Coalition </div>
   
