70e7b37816b0f5defdae366edc4a3bf5ea0f19c6
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 13:40:37 -0400

    Update enter_information.php

--- enter_information.php
+++ enter_information.php
@@ -1,27 +1,23 @@
 <?PHP include_once('header.php');  ?>
 
+  <div class='col-sm-12'><h1>Please enter your Name and ZIP Code as it appears on your Maryland Voter Registration</h1></div>
   
- <div class='col-sm-12'> please enter your name and zip code as it appears on your Maryland Voter Registration </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>First Name</h3></div>
   
-  <div class='col-sm-6'>first name </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>Last Name</h3></div>
   
-  <div class='col-sm-6'>last name </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><input> </div>
   
+  <div class='col-sm-6' style='height:50px; text-align:center;'><input> </div>
   
-    <div class='col-sm-6'><input> </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>House Number</h3></div>
   
-  <div class='col-sm-6'><input> </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>ZIP Code</h3></div>
   
-  <div class='col-sm-6'>house number</div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><input> </div>
   
-  <div class='col-sm-6'>zip code </div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><input> </div>
   
-  
-    <div class='col-sm-6'><input> </div>
-  
-  <div class='col-sm-6'><input> </div>
-  
-  <button type="button" class="btn btn-success">Submit</button>
-
+  <div class='col-sm-12'><button type="button" class="btn btn-success">Next</button></div>
 
 <?PHP include_once('footer.php');
