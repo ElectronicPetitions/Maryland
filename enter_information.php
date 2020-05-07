c114242d1ed94a91abee5b845250171e194eef09
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 22:39:59 -0400

    Update enter_information.php

--- enter_information.php
+++ enter_information.php
@@ -4,31 +4,31 @@ if (isset($_POST)){
   if (isset($_POST['web_first_name'])){
     if ($_POST['web_first_name'] != ''){
      $web_first_name = $_POST['web_first_name'];
-     setcookie("web_first_name", $web_first_name, time()+300);
+     setcookie("web_first_name", $web_first_name);
     }
   }
   $web_last_name='';
   if (isset($_POST['web_last_name'])){
     if ($_POST['web_last_name'] != ''){
      $web_last_name = $_POST['web_last_name'];
-     setcookie("web_last_name", $web_last_name, time()+300);
+     setcookie("web_last_name", $web_last_name);
     }
   }
   $web_house_number='';
   if (isset($_POST['web_house_number'])){
     if ($_POST['web_house_number'] != ''){
      $web_house_number = $_POST['web_house_number'];
-     setcookie("web_house_number", $web_house_number, time()+300);
+     setcookie("web_house_number", $web_house_number);
     }
   }
   $web_zip_code='';
   if (isset($_POST['web_zip_code'])){
     if ($_POST['web_zip_code'] != ''){
      $web_zip_code = $_POST['web_zip_code'];
-     setcookie("web_zip_code", $web_zip_code, time()+300);
+     setcookie("web_zip_code", $web_zip_code);
     }
   }
-  setcookie("web_name", $web_first_name.' '.$web_last_name, time()+300);
+  setcookie("web_name", $web_first_name.' '.$web_last_name);
   header('Location: is_the_information_correct.php');
 }
 include_once('header.php');
@@ -57,6 +57,7 @@ include_once('header.php');
   <div class='col-sm-12' style='height:50px; text-align:center;'><button type="submit" class="btn btn-success">Next</button></div>
 
   <div class='col-sm-12' style='height:50px; text-align:center;'><button type="reset" class="btn btn-warning">Clear</button></div>
+
 </form>
 
 <?PHP include_once('footer.php');
