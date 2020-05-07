44ece5817d3bba4b2b84b4a5ca3e3b535adb043d
Patrick McGuire <insidenothing@gmail.com>
2020-05-07 10:06:18 -0400

    Update hard_copy.php

--- hard_copy.php
+++ hard_copy.php
@@ -1,8 +1,13 @@
 <?php
+// imagettftext ( resource $image , float $size , float $angle , int $x , int $y , int $color , string $fontfile , string $text ) 
+// x how far from left
+// y how far from top
+
+
 //Set the Content Type
 header('Content-type: image/jpeg');
 
-// Create Image From Existing File
+// Create Image From Existing File -- going to have to make this a reqired size??? 2550x3300
 $jpg_image = imagecreatefromjpeg('files/Green-Party-petition-1-page-001.jpg');
 
 // Allocate A Color For The Text
@@ -11,11 +16,18 @@ $black = imagecolorallocate($jpg_image, 0, 0, 0);
 // Set Path to Font File
 $font_path = 'files/coolvetica rg.ttf';
 
-// Set Text to Be Printed On Image
-$text = "Signed My Name";
 
-// Print Text On Image
-imagettftext($jpg_image, 25, 0, 75, 300, $black, $font_path, $text);
+
+
+// County on Petition
+imagettftext($jpg_image, 40, 0, 90, 200, $black, $font_path, 'Baltimore');
+// OR
+// City Checkbox
+imagettftext($jpg_image, 25, 0, 85, 350, $black, $font_path, 'X');
+
+
+
+
 
 // Send Image to Browser
 imagejpeg($jpg_image);
