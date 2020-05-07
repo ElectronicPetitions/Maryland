162b6ce211715ce289e0ebbabda3b7053db0420a
Patrick McGuire <insidenothing@gmail.com>
2020-05-07 09:40:44 -0400

    Update petition.php

--- petition.php
+++ petition.php
@@ -7,5 +7,25 @@ if (isset($_POST['petition'])){
   $d = mysqli_fetch_array($r);
 }
 ?>
+<button onclick="myFunction()">Sign Now</button>
+
+
+
+<script>
+function myFunction() {
+  var txt;
+  var person = prompt("Please sign your name by typing :", "");
+  if (person == null || person == "") {
+    txt = "User cancelled the prompt.";
+  } else {
+    txt = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+</script>
+
 <div class='col-sm-12'><img class="img-responsive" src='<?PHP echo $d['petition_jpg'];?>'></div>
+
+<p id="demo"></p>
+
 <?PHP include_once('footer.php');
