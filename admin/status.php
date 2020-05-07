5c1a7aba94f638d7417c1c25e51ab7f9bb7ed0d6
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 21:24:35 -0400

    Update status.php

--- admin/status.php
+++ admin/status.php
@@ -9,6 +9,6 @@ include_once('header.php');
     $row_cnt = $result->num_rows;
     $c = $row_cnt / $expected;
     $p = number_format($c,2)*100;
-    echo '<h1>'.date('r').'</h1><h1>'.number_format($row_cnt).' out of '.number_format($expected).' '.$p.'% done</h1><title>'.$p.'% done</title>';
+    echo '<h1>'.date('r').'</h1><h1>'.number_format($row_cnt).' out of '.number_format($expected).' '.$p.'% done</h1><title>'.$p.'% loaded</title>';
 } 
 include_once('footer.php');  ?>
