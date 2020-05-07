de92e95d60b96edce70f5a22a1000d406d588b9e
Patrick McGuire <insidenothing@gmail.com>
2020-05-07 00:53:58 -0400

    Update petition.php

--- petition.php
+++ petition.php
@@ -1,3 +1,11 @@
-<?PHP include_once('header.php'); ?>
-  <div class='col-sm-12'><img class="img-responsive" src='files/Green-Party-petition-1-page-001.jpg'></div>
+<?PHP
+include_once('header.php'); 
+if (isset($_POST['petition'])){
+  $id = $_POST['petition'];
+  $q = "select * from petitions where petition_id = '$id'";
+  $r = $petition->query($q);
+  $d = mysqli_fetch_array($r);
+}
+?>
+<div class='col-sm-12'><img class="img-responsive" src='<?PHP echo $d['petition_jpg'];?>'></div>
 <?PHP include_once('footer.php');
