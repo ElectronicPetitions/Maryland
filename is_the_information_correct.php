00e1b93d6f6c7d5f37b7c23fd3d9bf4a34a7f5e6
Patrick McGuire <insidenothing@gmail.com>
2020-05-06 23:23:55 -0400

    Update is_the_information_correct.php

--- is_the_information_correct.php
+++ is_the_information_correct.php
@@ -46,7 +46,7 @@ if ($d['VTRID'] != ''){
   <div class='col-sm-2'>Zip</div><div class='col-sm-2'><?PHP echo $RESIDENTIALZIP5;?></div>
 </div>
 <div class='row'>
-  <div class='col-sm-6' style='height:50px; text-align:center;'><button type="button" class="btn btn-success onclick="window.location.href='eligible.php'">YES</button></div>
+  <div class='col-sm-6' style='height:50px; text-align:center;'><button type="button" class="btn btn-success" onclick="window.location.href='eligible.php'">YES</button></div>
   <div class='col-sm-6' style='height:50px; text-align:center;'><button type="button" class="btn btn-danger" onclick="window.location.href='reset.php'">NO</button></div>
 </div>
   
