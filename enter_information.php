9b738b78793f21582748268a6fac4fcc1a244d80
Patrick McGuire <insidenothing@gmail.com>
2020-05-07 12:17:32 -0400

    Update enter_information.php

--- enter_information.php
+++ enter_information.php
@@ -42,51 +42,33 @@ if (isset($_POST['web_first_name'])){
 }
 include_once('header.php');
 ?>
-
 <form method='POST'>
   <div class='row'>
-  <div class='col-sm-12' style='height:100px; text-align:center;'><h3>Please enter your Name and ZIP Code as it appears on your Maryland Voter Registration</h3></div>
+    <div class='col-sm-12' style='height:100px; text-align:center;'><h3>Please enter your Name and ZIP Code as it appears on your Maryland Voter Registration</h3></div>
   </div>
   <div class='row'>
-  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>First Name</h3></div>
-  
-  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>Last Name</h3></div>
+    <div class='col-sm-4' style='height:50px; text-align:center;'><h3>First Name</h3></div>
+    <div class='col-sm-4' style='height:50px; text-align:center;'><h3>Last Name</h3></div>
+    <div class='col-sm-4' style='height:50px; text-align:center;'><h3>Date of Birth</h3></div>
   </div>
   <div class='row'>
-  
-  <div class='col-sm-6' style='height:50px; text-align:center;'><input name='web_first_name'> </div>
-  
-  <div class='col-sm-6' style='height:50px; text-align:center;'><input name='web_last_name'> </div>
+    <div class='col-sm-4' style='height:50px; text-align:center;'><input name='web_first_name'></div>
+    <div class='col-sm-4' style='height:50px; text-align:center;'><input name='web_last_name'></div> 
+    <div class='col-sm-4' style='height:50px; text-align:center;'><input name='DOB' type="date"></div>
     </div>
   <div class='row'>
-
-  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>House Number</h3></div>
-  
-  <div class='col-sm-6' style='height:50px; text-align:center;'><h3>ZIP Code</h3></div>
-    </div>
-  <div class='row'>
-
-  <div class='col-sm-6' style='height:50px; text-align:center;'><input name='web_house_number'> </div>
-  
-  <div class='col-sm-6' style='height:50px; text-align:center;'><input name='web_zip_code'> </div>
-    </div>
-  
-  
-  <div class='row'>
-    <div class='col-sm-12' style='height:50px; text-align:center;'><h3>Date of Birth</h3> <input name='DOB' type="date"> </div>
-  </div>
-  
-  
-  <div class='row'>
-
-  <div class='col-sm-12' style='height:50px; text-align:center;'><button type="submit" class="btn btn-success">Next</button></div>
+    <div class='col-sm-6' style='height:50px; text-align:center;'><h3>House Number</h3></div>
+    <div class='col-sm-6' style='height:50px; text-align:center;'><h3>ZIP Code</h3></div>
   </div>
   <div class='row'>
-
-  <div class='col-sm-12' style='height:50px; text-align:center;'><button type="reset" class="btn btn-warning">Clear</button></div>
+    <div class='col-sm-6' style='height:50px; text-align:center;'><input name='web_house_number'> </div>
+    <div class='col-sm-6' style='height:50px; text-align:center;'><input name='web_zip_code'> </div>
   </div>
-  
-
+  <div class='row'>
+    <div class='col-sm-12' style='height:50px; text-align:center;'><button type="submit" class="btn btn-success">Next</button></div>
+  </div>
+  <div class='row'>
+    <div class='col-sm-12' style='height:50px; text-align:center;'><button type="reset" class="btn btn-warning">Clear</button></div>
+  </div>
 </form>
-
 <?PHP include_once('footer.php');
