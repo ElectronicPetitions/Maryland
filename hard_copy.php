262e4084193d7b38bb0534c001e3828cea86f2de
Patrick McGuire <insidenothing@gmail.com>
2020-05-07 10:37:09 -0400

    Update hard_copy.php

--- hard_copy.php
+++ hard_copy.php
@@ -27,13 +27,13 @@ imagettftext($jpg_image, 50, 0, 115, 365, $black, $font_path, 'X');
 
 // Slot 1 on Petition
 // name
-imagettftext($jpg_image, 50, 0, 350, 1050, $black, $font_path, 'Patrick Michael McGuire');
+imagettftext($jpg_image, 50, 0, 350, 1070, $black, $font_path, 'Patrick Michael McGuire');
 // address
-imagettftext($jpg_image, 50, 0, 350, 1350, $black, $font_path, '501 Spring Ave Lutherville 21093');
+imagettftext($jpg_image, 50, 0, 350, 1300, $black, $font_path, '501 Spring Ave Lutherville 21093');
 // date of birth
-imagettftext($jpg_image, 50, 0, 1800, 1050, $black, $font_path, '02 09 1980');
+imagettftext($jpg_image, 50, 0, 1800, 1070, $black, $font_path, '02 09 1980');
 // date signed
-imagettftext($jpg_image, 50, 0, 1800, 1250, $black, $font_path, '05 07 2020');
+imagettftext($jpg_image, 50, 0, 1800, 1150, $black, $font_path, '05 07 2020');
 
 
 
