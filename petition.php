56b829ee5d88dde498b8211aa140e570f7ffb453
Patrick McGuire <insidenothing@gmail.com>
2020-05-07 11:37:58 -0400

    Update petition.php

--- petition.php
+++ petition.php
@@ -7,7 +7,29 @@ if (isset($_POST['petition'])){
   $d = mysqli_fetch_array($r);
 }
 ?>
+<script>
+  $('#exampleModalCenterTitle').modal('show');
+</script>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Adopt Digital Signature</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Sign and Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
 <div class='col-sm-12'><img class="img-responsive" src='<?PHP echo $d['petition_jpg'];?>'></div>
 
 
