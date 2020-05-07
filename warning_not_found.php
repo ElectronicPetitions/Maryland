d5a301b5af1dc26693debbb92c047088f6dc13d9
Patrick McGuire <insidenothing@gmail.com>
2020-05-07 12:06:02 -0400

    Update warning_not_found.php

--- warning_not_found.php
+++ warning_not_found.php
@@ -2,10 +2,8 @@
 
   <div class='col-sm-12' style='height:100px; text-align:center;'><h2>Voter Information Not Found</h2></div>
 
-<div class='col-sm-12' style='height:100px; text-align:center;'>
-<pre>
-<?PHP print_r($_COOKIE); ?>
-</pre>
-</div>
+  <div class='col-sm-12' style='height:100px; text-align:center;'>
+    Say something about what to check...
+  </div>
 
 <?PHP include_once('footer.php');
