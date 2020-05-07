d5241492c0eb19c3a8c8bc2f2341680247e79356
Patrick McGuire <insidenothing@gmail.com>
2020-05-07 12:06:44 -0400

    Update warning_incomplete.php

--- warning_incomplete.php
+++ warning_incomplete.php
@@ -1,1 +1,9 @@
-tbd
+<?PHP include_once('header.php');  ?>
+
+  <div class='col-sm-12' style='height:100px; text-align:center;'><h2>Voter Information Incomplete</h2></div>
+
+  <div class='col-sm-12' style='height:100px; text-align:center;'>
+    Say something about why we need the info to validate...
+  </div>
+
+<?PHP include_once('footer.php');
