86f2f2cf257efb28a126a78db8654b908558bdd2
Patrick McGuire <insidenothing@gmail.com>
2020-05-12 17:43:55 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
   <div class='col-sm-12' style='text-align:center;'><hr> &COPY; 2020</div>
-   </div>
+  <div class='col-sm-12' style='text-align:center;'><?PHP if($_COOKIE['debug'] == 'on'){ ?> <pre><?PHP print_r($_GET); ?></pre><pre><?PHP print_r($_POST); ?></pre> <pre><?PHP print_r($_SERVER); ?></pre>  <?PHP } ?></div>
+ </div>
 </body>
