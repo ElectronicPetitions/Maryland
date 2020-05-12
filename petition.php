7f3ed8d1f1ea31ac71f1a6fd7b840b3c9296b4b6
Patrick McGuire <insidenothing@gmail.com>
2020-05-12 08:51:37 -0400

    Update petition.php

--- petition.php
+++ petition.php
@@ -55,7 +55,7 @@ if (isset($_POST['signed_name_as'])){
 
 <!-- TOP Modal -->
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">Type Your Signature to Sign Petition</h5>
@@ -78,7 +78,7 @@ if (isset($_POST['signed_name_as'])){
 
 <!-- Lower Modal -->
 <div class="modal fade" id="exampleModalLower" tabindex="-1" role="dialog" aria-labelledby="exampleModalLowerTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">Type Your Signature to Sign as Circulator</h5>
