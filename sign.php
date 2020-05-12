1c8d58738a767b0293e5b1dce32e9ce9a4695b24
Patrick McGuire <insidenothing@gmail.com>
2020-05-12 17:30:41 -0400

    Update sign.php

--- sign.php
+++ sign.php
@@ -14,5 +14,5 @@ $date_of_birth              = $_COOKIE['pDOB'];
 $signed_name_as_circulator  = $_POST['signed_name_as_circulator'];
 $contact_phone              = $_COOKIE['pPHONE'];
 $signature_status           = $_COOKIE['signature_status'];
-$petition->query("insert into signatures (VTRID,ip_address,date_of_birth,date_time_signed,petition_id,signed_name_as,signed_name_as_circulator,contact_phone,signature_status) values ('$VTRID','$ip','$date_of_birth',NOW(),'$petition_id','$signed_name_as','$signed_name_as_circulator','$contact_phone','$signature_status')");
+$petition->query("insert into signatures (VTRID,ip_address,date_of_birth,date_time_signed,petition_id,signed_name_as,signed_name_as_circulator,contact_phone,signature_status) values ('$VTRID','$ip','$date_of_birth',NOW(),'$petition_id','$signed_name_as','$signed_name_as_circulator','$contact_phone','$signature_status')") or die(mysqli_error($petition));
 header('Location: eligible.php');
