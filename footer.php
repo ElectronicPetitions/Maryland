51441cbd759a537cce3496de5d513fb502b82744
Patrick McGuire <insidenothing@gmail.com>
2020-05-12 09:21:54 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-  <div class='col-sm-12' style='text-align:center;'>&COPY; 2020 MGP</div>
-  <div class='col-sm-12' style='text-align:center;'><small><a target='_Blank' href='https://validator.w3.org/nu/?doc=https://www.mdwestserve.com<?PHP echo $_SERVER['SCRIPT_NAME'];?>'><?PHP echo $_SERVER['SCRIPT_NAME'];?></a></small></div>
-  </div>
+  <div class='col-sm-12' style='text-align:center;'><hr> &COPY; 2020</div>
+   </div>
 </body>
