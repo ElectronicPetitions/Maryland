cae07d22001664c036d08b9d2ff54cfe7003120d
Patrick McGuire <insidenothing@gmail.com>
2020-05-12 18:08:28 -0400

    Update header.php

--- header.php
+++ header.php
@@ -33,8 +33,14 @@ if ($_GET['debug'] == 'off'){
       <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/not_a_registered_voter.php'){ echo "class='active'"; } ?> ><a>2: How to Register?</a></li>
       <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/enter_information.php'){ echo "class='active'"; } ?> ><a>3: Enter Information</a></li>
       <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/is_the_information_correct.php'){ echo "class='active'"; } ?> ><a>4: Confirm Information</a></li>    
+      <?PHP if (empty($_COOIIE['invite'])){ ?>
       <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/eligible.php'){ echo "class='active'"; } ?> ><a>5: Select Petition</a></li>
       <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/petition.php'){ echo "class='active'"; } ?> ><a>6: Sign Petition</a></li>
+      <?PHP }else{ ?>
+      <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/petition.php'){ echo "class='active'"; } ?> ><a>5: Sign Petition</a></li>
+      <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/MarylandElectronicPetitionSignature/eligible.php'){ echo "class='active'"; } ?> ><a>6: Select Petition</a></li>
+      <?PHP } ?>
+      
       <?PHP if ($_COOKIE['web_name'] != ''){ ?>
         <li role='presentation'><a href="reset.php">RESET</a></li>
       <?PHP } ?>
