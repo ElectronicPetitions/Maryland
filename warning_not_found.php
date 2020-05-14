5625b4c2b68ba5ece51656ff15e67c9d5a20e349
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 11:57:48 -0400

    Update warning_not_found.php

--- warning_not_found.php
+++ warning_not_found.php
@@ -3,7 +3,7 @@ $qX = "select * from website_text where id = '5'";
  $rX = $petition->query($qX);
  $dX = mysqli_fetch_array($rX);
 ?>
-<script>document.title = "MEPS - Warning Incomplete";</script>
+<script>document.title = "MEPS - Warning Not Found";</script>
   <div class='col-sm-12' style='height:100px; text-align:center;'><h2><?PHP echo $dX['text_title'];?></h2></div>
 
   <div class='col-sm-12' style='height:100px; text-align:center;'>
