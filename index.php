a0b7fc69d679fa18d9c3242f012a859b5599f1c0
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 11:36:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,13 @@ if ($_COOKIE['invite'] != ''){
  $title = 'MEPS ('.strtoupper($_COOKIE['invite']).') - Are you Registered?'; 
 }
 include_once('header.php');
+
+ $q = "select * from website_text where id = '1'";
+ $r = $petition->query($q);
+ $d = mysqli_fetch_array($r);
 ?>
   <script>document.title = "<?PHP echo $title;?>";</script>
-  <div class='col-sm-12' style='height:100px; text-align:center;'><h2>Are you a Registered Maryland Voter?</h2></div>
+  <div class='col-sm-12' style='height:100px; text-align:center;'><h2><?PHP echo $d['text_title'];?></h2><p><?PHP echo $d['text_block'];?></p></div>
   
   <div class='col-sm-6' style='height:100px; text-align:center;'><button type="button" class="btn btn-success" onclick="window.location.href='enter_information.php'">YES</button></div>
   
