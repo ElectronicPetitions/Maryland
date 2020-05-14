883fba2d1d6d21a8f955b487ab8635a2c01ee5e6
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 13:47:44 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,3 +14,15 @@ if ($_COOKIE['invite'] != ''){
 </div>
  </div>
 </body>
+<script type="text/javascript">
+    window.doorbellOptions = {
+        "id": "10512",
+        "appKey": "4wEnfKuAdj4WlHgKaxLMrAbYn8aKdFhtYVnpbRPkG4AbA7SH7KcgiRvdZyWmwxnw"
+    };
+    (function(w, d, t) {
+        var hasLoaded = false;
+        function l() { if (hasLoaded) { return; } hasLoaded = true; window.doorbellOptions.windowLoaded = true; var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/'+window.doorbellOptions['id']+'?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g); }
+        if (w.attachEvent) { w.attachEvent('onload', l); } else if (w.addEventListener) { w.addEventListener('load', l, false); } else { l(); }
+        if (d.readyState == 'complete') { l(); }
+    }(window, document, 'script'));
+</script>
