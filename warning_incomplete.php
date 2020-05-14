878267440a030a2160c35f4a1799b0fd735a4347
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 11:56:42 -0400

    Update warning_incomplete.php

--- warning_incomplete.php
+++ warning_incomplete.php
@@ -1,9 +1,13 @@
-<?PHP include_once('header.php');  ?>
-
-  <div class='col-sm-12' style='height:100px; text-align:center;'><h2>Voter Information Incomplete</h2></div>
+<?PHP include_once('header.php');  
+$qX = "select * from website_text where id = '4'";
+ $rX = $petition->query($qX);
+ $dX = mysqli_fetch_array($rX);
+?>
+<script>document.title = "MEPS - Warning Incomplete";</script>
+  <div class='col-sm-12' style='height:100px; text-align:center;'><h2><?PHP echo $dX['text_title'];?></h2></div>
 
   <div class='col-sm-12' style='height:100px; text-align:center;'>
-    Say something about why we need the info to validate...
+    <?PHP echo $dX['text_block'];?>
   </div>
 
 <?PHP include_once('footer.php');
