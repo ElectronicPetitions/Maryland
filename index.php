bab8daf29fb52c7c6fe70413ebd7029429808287
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 08:29:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,13 @@ if (isset($_GET['invite'])){
   setcookie("invite", $_GET['invite']);
   header('Location: index.php');
 }
+$title = 'MEPS - Are you Registered?';
+if ($_COOKIE['invite'] != ''){
+ $title = 'MEPS ('.$_COOKIE['invite'].') - Are you Registered?'; 
+}
 include_once('header.php');
 ?>
-  <script>document.title = "MEPS - Are you Registered?";</script>
+  <script>document.title = "<?PHP echo $title;?>";</script>
   <div class='col-sm-12' style='height:100px; text-align:center;'><h2>Are you a Registered Maryland Voter?</h2></div>
   
   <div class='col-sm-6' style='height:100px; text-align:center;'><button type="button" class="btn btn-success" onclick="window.location.href='enter_information.php'">YES</button></div>
