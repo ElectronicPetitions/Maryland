ca0b6912b426c4f92429fa0e86aaf10b9da058c2
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 08:00:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (isset($_GET['invite'])){
 }
 include_once('header.php');
 ?>
-  
+  <script>document.title = "MEPS - Are you Registered?";</script>
   <div class='col-sm-12' style='height:100px; text-align:center;'><h2>Are you a Registered Maryland Voter?</h2></div>
   
   <div class='col-sm-6' style='height:100px; text-align:center;'><button type="button" class="btn btn-success" onclick="window.location.href='enter_information.php'">YES</button></div>
