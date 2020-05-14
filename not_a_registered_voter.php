6d99c3c6f1e4a89cc10f3c87c56557745e476beb
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 11:49:02 -0400

    Update not_a_registered_voter.php

--- not_a_registered_voter.php
+++ not_a_registered_voter.php
@@ -1,8 +1,9 @@
-<?PHP include_once('header.php');  ?>
+<?PHP include_once('header.php'); 
+$qX = "select * from website_text where id = '3'";
+ $rX = $petition->query($qX);
+ $dX = mysqli_fetch_array($rX);?>
 <script>document.title = "MEPS - How to Register?";</script>
-<div class='col-sm-12' style='height:75px; text-align:center;'><h2>You must be a registered voter in the State of Maryland to sign this petition.</h2></div>
-
-<div class='col-sm-12' style='height:75px; text-align:center;'><h3>If you are eligible</h3></div>
+<div class='col-sm-12' style='height:75px; text-align:center;'><h3><?PHP echo $dX['text_title'];?></h3><p><?PHP echo $dX['text_block'];?></p></div>
 
 <div class='col-sm-12' style='height:50px; text-align:center;'><button type="button" class="btn btn-success" onclick="window.location.href='https://voterservices.elections.maryland.gov/OnlineVoterRegistration/InstructionsStep1'">You Can Register Online Here</button></div>
 
