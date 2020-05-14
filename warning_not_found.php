ce49cf798afe7cc049c8fdd0d9cbc54e3c73a0ad
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 11:57:35 -0400

    Update warning_not_found.php

--- warning_not_found.php
+++ warning_not_found.php
@@ -1,9 +1,13 @@
-<?PHP include_once('header.php');  ?>
-
-  <div class='col-sm-12' style='height:100px; text-align:center;'><h2>Voter Information Not Found</h2></div>
+<?PHP include_once('header.php');  
+$qX = "select * from website_text where id = '5'";
+ $rX = $petition->query($qX);
+ $dX = mysqli_fetch_array($rX);
+?>
+<script>document.title = "MEPS - Warning Incomplete";</script>
+  <div class='col-sm-12' style='height:100px; text-align:center;'><h2><?PHP echo $dX['text_title'];?></h2></div>
 
   <div class='col-sm-12' style='height:100px; text-align:center;'>
-    Say something about what to check...
+    <?PHP echo $dX['text_block'];?>
   </div>
 
 <?PHP include_once('footer.php');
