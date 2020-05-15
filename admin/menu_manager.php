512a1f9e591b367ca44e127327e4c261aa759768
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 23:28:02 -0400

    Update menu_manager.php

--- admin/menu_manager.php
+++ admin/menu_manager.php
@@ -1,1 +1,2 @@
-[manager menu]
+ <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/manage_petitions.php'){ echo "class='active'"; } ?> ><a href="manage_petitions.php">Set Up Petitions</a></li>
+ <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/users.php'){ echo "class='active'"; } ?> ><a href="users.php">Set Up Users</a></li>
