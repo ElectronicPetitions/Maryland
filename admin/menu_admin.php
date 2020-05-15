0245775b38abab3f632442afaf29c9e677fe4ed4
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 23:51:35 -0400

    Update menu_admin.php

--- admin/menu_admin.php
+++ admin/menu_admin.php
@@ -2,5 +2,3 @@
 <li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/status.php'){ echo "class='active'"; } ?> ><a href="status.php">Voter Data File Status</a></li>
 <li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/admins.php'){ echo "class='active'"; } ?> ><a href="admins.php">Set Up Admins</a></li>
 <li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/managers.php'){ echo "class='active'"; } ?> ><a href="managers.php">Set Up Managers</a></li>          
-<li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/backup.php'){ echo "class='active'"; } ?> ><a href="backup.php">backup</a></li>
-<li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/restore.php'){ echo "class='active'"; } ?> ><a href="restore.php">restore</a></li>
