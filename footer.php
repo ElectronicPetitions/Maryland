398b3c750512cad6bb76380e5cd60c554014b9f0
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 20:59:32 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,6 +8,47 @@ if ($_COOKIE['invite'] != ''){
 <div class='row'>
  <div class='col-sm-12' style='text-align:center;'> &COPY; 2020 <?PHP echo $copy;?></div>
 </div>
+
+
+  <meta property="og:url"           content="http://md-petition.com/index.php" />
+  <meta property="og:type"          content="website" />
+  <meta property="og:title"         content="Maryland Electronic Petition Software" />
+  <meta property="og:description"   content="Socially Distant Petitions" />
+  <meta property="og:image"         content="http://md-petition.com/files/Flag_of_Maryland.svg" />
+
+
+<div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+
+<div class='row'>
+ <div class='col-sm-4' style='text-align:center;'> Share MGP
+  <div class="fb-share-button" 
+     data-href="http://md-petition.com/index.php?invite=MGP" 
+     data-layout="button_count">
+   </div>
+ </div>
+ <div class='col-sm-4' style='text-align:center;'> Share MLP 
+  <div class="fb-share-button" 
+     data-href="http://md-petition.com/index.php?invite=MLP" 
+     data-layout="button_count">
+   </div>
+ </div>
+ <div class='col-sm-4' style='text-align:center;'> Share BTEC
+  <div class="fb-share-button" 
+     data-href="http://md-petition.com/index.php?invite=BTEC" 
+     data-layout="button_count">
+   </div>
+ </div>
+</div>
+
+
   <div class='col-sm-12' style='text-align:center;'>
    <?PHP if($_COOKIE['debug'] == 'on'){ ?> 
     <pre><?PHP print_r($_GET); ?></pre>
