3ad30165fd777bfafc3a7d941c2fa3f34c0f6127
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 23:46:51 -0400

    Update footer.php

--- admin/footer.php
+++ admin/footer.php
@@ -1,4 +1,4 @@
-  <div class='col-sm-12' style='text-align:center;'>&COPY; 2020 MGP</div>
+  <div class='col-sm-12' style='text-align:center;'>&COPY; 2020 Patrick McGuire - baltimorehacker@gmail.com - 443-386-2584 (leave message)</div>
   <div class='col-sm-12' style='text-align:center;'><small><a target='_Blank' href='https://validator.w3.org/nu/?doc=https://www.mdwestserve.com<?PHP echo $_SERVER['SCRIPT_NAME'];?>'><?PHP echo $_SERVER['SCRIPT_NAME'];?></a></small></div>
   </div>
 </body>
