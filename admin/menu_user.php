d36003c2ec84ce8c27e54e5f528414ec1ecd33ab
Patrick McGuire <insidenothing@gmail.com>
2020-05-14 23:49:41 -0400

    Update menu_user.php

--- admin/menu_user.php
+++ admin/menu_user.php
@@ -1,2 +1,2 @@
-<li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/report.php'){ echo "class='active'"; } ?> ><a href="report.php">Reports</a></li>
+<li role='presentation' style='background-color:lightyellow;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/report.php'){ echo "class='active'"; } ?> ><a href="report.php">Reports</a></li>
       
