737e8418b6ae79b0393ee57376424de0bb84dc30
Patrick McGuire <insidenothing@gmail.com>
2020-05-15 09:28:58 -0400

    style buttons

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ include_once('header.php');
   <script>document.title = "<?PHP echo $title;?>";</script>
   <div class='col-sm-12' style='height:100px; text-align:center;'><h2><?PHP echo $d['text_title'];?></h2><p><?PHP echo $d['text_block'];?></p></div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><button type="button" class="btn btn-success" onclick="window.location.href='enter_information.php'">YES</button></div>
+  <div class='col-sm-6' style='height:100px; text-align:center;'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='enter_information.php'">YES</button></div>
   
-  <div class='col-sm-6' style='height:100px; text-align:center;'><button type="button" class="btn btn-danger" onclick="window.location.href='not_a_registered_voter.php'">NO</button> </div>
+  <div class='col-sm-6' style='height:100px; text-align:center;'><button type="button" class="btn btn-danger btn-lg btn-block" onclick="window.location.href='not_a_registered_voter.php'">NO</button> </div>
   
 <?PHP include_once('footer.php');
