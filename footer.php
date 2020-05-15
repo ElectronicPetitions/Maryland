7c9b28e19bee970ba01aece5553e6115a31c9ca7
Patrick McGuire <insidenothing@gmail.com>
2020-05-15 10:00:48 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -35,13 +35,13 @@ if ($_COOKIE['invite'] != ''){
 
 
 <footer class="site-footer">
- <center>&COPY; 2020 <?PHP echo $copy;?>< br>
- <table><tr>
+ <center>&COPY; 2020 <?PHP echo $copy;?>
+ <table border="1" cellpadding="0" cellspacing="0"><tr>
  <?PHP
  $q2 = "SELECT * FROM petitions";
  $r2 = $petition->query($q2);
  while($d2 = mysqli_fetch_array($r2)){
-  echo "<td>Share $d2[petition_name]<br>
+  echo "<td><small>$d2[petition_name]<small><br>
   <div class=\"fb-share-button\" 
      data-href=\"http://md-petition.com/index.php?invite=$d2[web_short_name]\" 
      data-layout=\"button_count\">
