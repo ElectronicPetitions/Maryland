4c5288083086df9223ccd73d58c4cb121616db22
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 16:08:59 -0400

    Update manager_home.php

--- admin/manager_home.php
+++ admin/manager_home.php
@@ -7,5 +7,37 @@ if ($_COOKIE['level'] == 'user'){
 }
 include_once('header.php');
 slack_general('ADMIN: Manager Home Page Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+$group_id = $_COOKIE['group_id'];
+?>
+<h1>Managers</h1>
+<?PHP
+$q="SELECT * FROM users where sec_level = 'manager' and group_id = '$group_id'";
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+ echo "<li>$d[id] $d[email] $d[name]</li>"; 
+}
+?>
+<h1>Users</h1>
+<?PHP
+$q="SELECT * FROM users where sec_level = 'user' and group_id = '$group_id'";
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+ echo "<li>$d[id] $d[email] $d[name]</li>"; 
+}
+?>
+
+<h1>Petitions</h1>
+<?PHP
+$q="SELECT * FROM petitions where group_id = '$group_id'";
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+ echo "<li>$d[petition_id] $d[web_short_name] $d[web_color] $d[group_id] $d[petition_name] $d[eligibleVoterListField] $d[eligibleVoterListEquals] $d[eligibleVoterListEnforce]</li>"; 
+}
+?>
+
+
+
+
+
 
 include_once('footer.php');
