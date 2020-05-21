5747852da646f40614cfb60090039ff0f30e4424
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 12:09:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ slack_general('Home Page Loaded ('.$_COOKIE['invite'].')','md-petition');
 ?>
 <script>document.title = "<?PHP echo $title;?>";</script>
 <div class='row'>
-  <div class='col-sm-12' style='text-align:center;'><h1><?PHP echo $d['text_title'];?></h1><h2><?PHP echo $d['text_block'];?></h2></div>
+  <div class='col-sm-10' style='text-align:center;'><h1><?PHP echo $d['text_title'];?></h1><h2><?PHP echo $d['text_block'];?></h2></div>
  </div> 
 <div class='row'>
-  <div class='col-sm-6' style='text-align:center;'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='enter_information.php'">YES</button></div>
-  <div class='col-sm-6' style='text-align:center;'><button type="button" class="btn btn-danger btn-lg btn-block" onclick="window.location.href='not_a_registered_voter.php'">NO</button></div>
+  <div class='col-sm-5' style='text-align:center;'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='enter_information.php'">YES</button></div>
+  <div class='col-sm-5' style='text-align:center;'><button type="button" class="btn btn-danger btn-lg btn-block" onclick="window.location.href='not_a_registered_voter.php'">NO</button></div>
  </div> 
 <?PHP include_once('footer.php');
