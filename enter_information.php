f62d901f8b9ad545114c0f18ed7d687f3f6e66d6
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 09:22:06 -0400

    Update enter_information.php

--- enter_information.php
+++ enter_information.php
@@ -83,7 +83,7 @@ slack_general('Entering Information','md-petition');
         <div class='col-sm-6'>
             <div class="form-group">
                 <div class='input-group date' id='datetimepicker3'>
-                    <input type='text' class="form-control" />
+                    <input name='DOB' type='text' class="form-control" />
                     <span class="input-group-addon">
                         <span class="glyphicon glyphicon-time"></span>
                     </span>
