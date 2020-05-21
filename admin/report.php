d156fc32c374ab5572c975302118f761c6f45c9c
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 17:07:21 -0400

    Update report.php

--- admin/report.php
+++ admin/report.php
@@ -5,13 +5,17 @@ slack_general('ADMIN: Reports Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level']
 $group_id = $_COOKIE['group_id'];
 $q="SELECT * FROM petitions where group_id = '$group_id' and admin_status = 'approved'";
 $r = $petition->query($q);
+$hide = array();
 while($d = mysqli_fetch_array($r)){
   echo "<h1>$d[petition_name]</h1>";
   $pID = $d['petition_id'];
-  $q2="SELECT * FROM signatures where petition_id = '$pID'";
+  $q2="SELECT * FROM signatures where petition_id = '$pID' order by id desc";
   $r2 = $petition->query($q2);
   while($d2 = mysqli_fetch_array($r2)){
-    echo "<li>$d2[date_time_signed] $d2[signed_name_as] $d2[signed_name_as_circulator] $d2[contact_phone]</li>";
+    if (!in_array($d2['VTRID'], $hide)) {
+      $hide[] = $d2['VTRID'];
+      echo "<li>$d2[date_time_signed] $d2[signed_name_as] $d2[signed_name_as_circulator] $d2[contact_phone]</li>";
+    }
   }
 }
 
