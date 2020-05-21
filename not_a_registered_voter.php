7e63cad8b6c136b70f5631ec0f689a489cc19f53
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 12:27:44 -0400

    Update not_a_registered_voter.php

--- not_a_registered_voter.php
+++ not_a_registered_voter.php
@@ -1,12 +1,17 @@
-<?PHP include_once('header.php'); 
+<?PHP 
+include_once('header.php'); 
+slack_general('Not a registered voter ('.$_COOKIE['invite'].')','md-petition');
 $qX = "select * from website_text where id = '3'";
- $rX = $petition->query($qX);
- $dX = mysqli_fetch_array($rX);?>
+$rX = $petition->query($qX);
+$dX = mysqli_fetch_array($rX);?>
 <script>document.title = "MEPS - How to Register?";</script>
-<div class='col-sm-12' style='height:75px; text-align:center;'><h3><?PHP echo $dX['text_title'];?></h3><p><?PHP echo $dX['text_block'];?></p></div>
-
-<div class='col-sm-12' style='height:50px; text-align:center;'><button type="button" class="btn btn-success" onclick="window.location.href='https://voterservices.elections.maryland.gov/OnlineVoterRegistration/InstructionsStep1'">You Can Register Online Here</button></div>
-
-<div class='col-sm-12' style='height:50px; text-align:center;'><small>https://voterservices.elections.maryland.gov/OnlineVoterRegistration/InstructionsStep1</small></div>
-
+<div class='row'>
+ <div class='col-sm-10' style='text-align:center;'><h1><?PHP echo $dX['text_title'];?></h1><h2><?PHP echo $dX['text_block'];?></h2></div>
+</div>
+<div class='row'>
+ <div class='col-sm-10' style='text-align:center;'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='https://voterservices.elections.maryland.gov/OnlineVoterRegistration/InstructionsStep1'">You Can Register Online Here</button></div>
+</div>
+<div class='row'>
+ <div class='col-sm-10' style='text-align:center;'><h3>https://voterservices.elections.maryland.gov/OnlineVoterRegistration/InstructionsStep1</h3></div>
+</div>
 <?PHP include_once('footer.php');
