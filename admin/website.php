d1cfbf6ebb64c5d27553503e85bca280f283bc75
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 14:11:25 -0400

    Update website.php

--- admin/website.php
+++ admin/website.php
@@ -1,1 +1,20 @@
-website.php
+<?PHP 
+
+include_once('security.php');
+
+include_once('header.php');
+slack_general('ADMIN: Website Text Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+?>
+
+<h1>Website Text</h1>
+
+
+
+<pre>
+<?PHP print_r($_COOKIE); ?>
+</pre>
+
+
+<?PHP
+include_once('footer.php');
+?>
