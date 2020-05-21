246f1870317770483de228f41449079bc6d218c1
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 14:15:04 -0400

    Update groups.php

--- admin/groups.php
+++ admin/groups.php
@@ -1,1 +1,20 @@
-groups.php
+<?PHP 
+
+include_once('security.php');
+
+include_once('header.php');
+slack_general('ADMIN: Group Manager Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+?>
+
+<h1>Group Manager</h1>
+
+
+
+<pre>
+<?PHP print_r($_COOKIE); ?>
+</pre>
+
+
+<?PHP
+include_once('footer.php');
+?>
