fdbb15dae45e605af2eb4b39e8644cfa175d7c7f
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 14:11:48 -0400

    Update admins.php

--- admin/admins.php
+++ admin/admins.php
@@ -1,1 +1,20 @@
-admins.php
+<?PHP 
+
+include_once('security.php');
+
+include_once('header.php');
+slack_general('ADMIN: admin manager ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+?>
+
+<h1>admin manager</h1>
+
+
+
+<pre>
+<?PHP print_r($_COOKIE); ?>
+</pre>
+
+
+<?PHP
+include_once('footer.php');
+?>
