f612f8801096f332cc6de3169db670e194381437
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 11:25:43 -0400

    Update warning_not_found.php

--- warning_not_found.php
+++ warning_not_found.php
@@ -1,6 +1,21 @@
 <?PHP 
+/* delete ALL cookies */
+foreach ( $_COOKIE as $key => $value ){
+	unset($_COOKIE[$key]);
+	setcookie($key, '', time() - 3600, '/'); 
+}
+// unset cookies
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
 include_once('header.php');  
-slack_general('Enter Information ('.$_COOKIE['invite'].')','md-petition');
+slack_general('Warning Not Found ('.$_COOKIE['invite'].')','md-petition');
 $qX = "select * from website_text where id = '5'";
 $rX = $petition->query($qX);
 $dX = mysqli_fetch_array($rX);
