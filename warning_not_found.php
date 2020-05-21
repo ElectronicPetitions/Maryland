833a22d37209cf74aefedd6af5fefe0f2314b795
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 11:24:27 -0400

    Update warning_not_found.php

--- warning_not_found.php
+++ warning_not_found.php
@@ -7,9 +7,9 @@ $dX = mysqli_fetch_array($rX);
 ?>
 <script>document.title = "MEPS - Warning Not Found";</script>
 <div class='row'>
- <div class='col-sm-10' style='height:100px; text-align:center;'><h1><?PHP echo $dX['text_title'];?></h1></div>
+ <div class='col-sm-10' style='text-align:center;'><h1><?PHP echo $dX['text_title'];?></h1></div>
 </div>
 <div class='row'>
- <div class='col-sm-10' style='height:100px; text-align:center;'><h2><?PHP echo $dX['text_block'];?></h2></div>
+ <div class='col-sm-10' style='text-align:center;'><h2><?PHP echo $dX['text_block'];?></h2></div>
 </div>
 <?PHP include_once('footer.php');
