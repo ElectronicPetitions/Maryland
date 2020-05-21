50f53cd010252699e3eb17c01edb81357d27116c
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 15:03:28 -0400

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,11 +3,11 @@ include_once('../slack.php');
 include_once('security.php');
 if ($_COOKIE['level'] == 'user'){
   slack_general('ADMIN: Redirect User Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
-  header('Location user_home.php');
+  header('Location: user_home.php');
 }
 if ($_COOKIE['level'] == 'manager'){
   slack_general('ADMIN: Redirect Manager Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
-  header('Location manager_home.php');
+  header('Location: manager_home.php');
 }
 
 include_once('header.php');
