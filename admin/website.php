9f0aeed2a381d8c142697d1dd832afe37b5cfa05
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 14:56:44 -0400

    Update website.php

--- admin/website.php
+++ admin/website.php
@@ -1,20 +1,25 @@
 <?PHP 
-
 include_once('security.php');
-
+if ($_COOKIE['level'] == 'user'){
+  slack_general('ADMIN: Redirect User Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+  header('Location: user_home.php');
+}
+if ($_COOKIE['level'] == 'manager'){
+  slack_general('ADMIN: Redirect Manager Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+  header('Location: manager_home.php');
+}
 include_once('header.php');
 slack_general('ADMIN: Website Text Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
 ?>
 
 <h1>Website Text</h1>
-
-
-
-<pre>
-<?PHP print_r($_COOKIE); ?>
-</pre>
-
-
 <?PHP
+ $q2 = "SELECT * FROM website_text";
+ $r2 = $petition->query($q2);
+ while($d2 = mysqli_fetch_array($r2)){
+  echo "<h2>$d2[text_title]</h2><div>$d2[text_block]</div>";
+}
+
+
 include_once('footer.php');
 ?>
