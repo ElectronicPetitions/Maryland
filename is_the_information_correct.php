bda3f5d8bb0c303314c0d5aa4aeaecda56f61ebb
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 10:43:34 -0400

    Update is_the_information_correct.php

--- is_the_information_correct.php
+++ is_the_information_correct.php
@@ -45,36 +45,40 @@ $qX = "select * from website_text where id = '6'";
     <div class='col-sm-10' style='height:100px; text-align:center;'><h1><?PHP echo $dX['text_title'];?></h1><h2><?PHP echo $dX['text_block'];?></h2></div>
 </div>
 <div class='row'>
-  <div class='col-sm-5' style='text-align:center;'><h2>First Name</h2></div>
-  <div class='col-sm-5' style='text-align:center;'><h2><?PHP echo $FIRSTNAME;?></h2></div>
+  <div class='col-sm-5' style='text-align:right;'><h2>First Name</h2></div>
+  <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $FIRSTNAME;?></h2></div>
 </div>
 <div class='row'>
-  <div class='col-sm-5' style='text-align:center;'><h2>Middle Name</h2></div>
-  <div class='col-sm-5' style='text-align:center;'><h2><?PHP echo $MIDDLENAME;?><h2></div>
+  <div class='col-sm-5' style='text-align:right;'><h2>Middle Name</h2></div>
+  <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $MIDDLENAME;?><h2></div>
 </div>
 <div class='row'>
-  <div class='col-sm-5' style='text-align:center;'><h2>Last Name</h2></div>
-  <div class='col-sm-5' style='text-align:center;'><h2><?PHP echo $LASTNAME;?></h2></div>
+  <div class='col-sm-5' style='text-align:right;'><h2>Last Name</h2></div>
+  <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $LASTNAME;?></h2></div>
 </div>
 <div class='row'>
-  <div class='col-sm-5' style='text-align:center;'><h2>Date of Birth</h2></div>
-  <div class='col-sm-5' style='text-align:center;'><h2><?PHP echo $DOB;?></h2></div>
+  <div class='col-sm-5' style='text-align:right;'><h2>Date of Birth</h2></div>
+  <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $DOB;?></h2></div>
 </div>
 <div class='row'>
-  <div class='col-sm-5' style='text-align:center;'><h2>Full Addresss</h2></div>
-  <div class='col-sm-5' style='text-align:center;'><h2><?PHP echo $ADDRESS;?></h2></div>
+  <div class='col-sm-5' style='text-align:right;'><h2>Full Addresss</h2></div>
+  <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $ADDRESS;?></h2></div>
 </div>
 <div class='row'>
-  <div class='col-sm-5' style='text-align:center;'><h2>Phone</h2></div>
-  <div class='col-sm-5' style='text-align:center;'><h2><?PHP echo $PHONE;?></h2></div>
+  <div class='col-sm-5' style='text-align:right;'><h2>Phone</h2></div>
+  <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $PHONE;?></h2></div>
 </div>
 <div class='row'>
-  <div class='col-sm-2'><h2>City</h2></div>
-  <div class='col-sm-2'><h2><?PHP echo $RESIDENTIALCITY;?></h2></div>
-  <div class='col-sm-2'><h2>County</h2></div>
-  <div class='col-sm-2'><h2><?PHP echo $COUNTY;?></h2></div>
-  <div class='col-sm-1'><h2>Zip</h2></div>
-  <div class='col-sm-1'><h2><?PHP echo $RESIDENTIALZIP5;?></h2></div>
+  <div class='col-sm-5' style='text-align:right;'><h2>City</h2></div>
+  <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $RESIDENTIALCITY;?></h2></div>
+</div>
+<div class='row'> 
+  <div class='col-sm-5' style='text-align:right;'><h2>County</h2></div>
+  <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $COUNTY;?></h2></div>
+</div>
+<div class='row'>
+  <div class='col-sm-5' style='text-align:right;'><h2>Zip</h2></div>
+  <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $RESIDENTIALZIP5;?></h2></div>
 </div>
 <div class='row'>
   <div class='col-sm-5'><button type="button" class="btn btn-danger btn-lg btn-block" onclick="window.location.href='reset.php'">NO</button></div>
