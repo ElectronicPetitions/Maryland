33242fed18582616d2ecedd4de9ad9d0eae9aee3
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 16:01:25 -0400

    Update groups.php

--- admin/groups.php
+++ admin/groups.php
@@ -1,19 +1,37 @@
 <?PHP 
-
+include_once('../slack.php');
 include_once('security.php');
-
+if ($_COOKIE['level'] == 'user'){
+  slack_general('ADMIN: Redirect User Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+  header('Location: user_home.php');
+}
+if ($_COOKIE['level'] == 'manager'){
+  slack_general('ADMIN: Redirect Manager Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+  header('Location: manager_home.php');
+}
 include_once('header.php');
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['group_id']) ){
+  $name = $petition->real_escape_string($_POST['name']);
+  $petition->query("insert into groups (name) values ('$name') ");
+}
 slack_general('ADMIN: Group Manager Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
 ?>
 
-<h1>Group Manager</h1>
+<h1>Groups</h1>
+<?PHP
+$q="SELECT * FROM groups";
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+ echo "<li>$d[id] $d[name]</li>"; 
+}
+?>
 
 
-
-<pre>
-<?PHP print_r($_COOKIE); ?>
-</pre>
-
+<h1>New Manager</h1>
+<form method='post'>
+  group name <input name='name' required>
+  <input type='submit'>
+</form>
 
 <?PHP
 include_once('footer.php');
