a5d1f6b6d0afc71ca3b8bb1dc2b10d4539edd1b4
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 15:30:20 -0400

    Update managers.php

--- admin/managers.php
+++ admin/managers.php
@@ -10,6 +10,13 @@ if ($_COOKIE['level'] == 'manager'){
   header('Location: manager_home.php');
 }
 include_once('header.php');
+if(isset($_POST['name']) && isset($_POST['email']) ){
+  $name = $petition->real_escape_string($_POST['name']);
+  $email = $petition->real_escape_string($_POST['email']);
+  $petition->query("insert into users (email,name,group_id,sec_level) values () ");
+}
+
+
 slack_general('ADMIN: managers.php ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
 ?>
 
@@ -21,7 +28,21 @@ while($d = mysqli_fetch_array($r)){
  echo "<li>$d[id] $d[email] $d[name] $d[group_id] $d[sec_level]</li>"; 
 }
 ?>
-
+<h1>Groups</h1>
+<?PHP
+$q="SELECT * FROM groups";
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+ echo "<li>$d[id] $d[name]</li>"; 
+}
+?>
+<h1>New Manager</h1>
+<form method='post'>
+  name <input name='name'>
+  email <input name='email'>
+  group_id <input name='group_id'>
+  <input type='submit'>
+</form>
 
 <?PHP
 include_once('footer.php');
