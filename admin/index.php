f628bd2d6beda0c1a51a71a54ba08eaa6b96cfaf
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 14:35:06 -0400

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,17 @@
 <?PHP 
-
+include_once('../slack.php');
 include_once('security.php');
+if ($_COOKIE['level'] == 'user'){
+  slack_general('ADMIN: Redirect User Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+  header('Location user_home.php');
+}
+if ($_COOKIE['level'] == 'manager'){
+  slack_general('ADMIN: Redirect Manager Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+  header('Location manager_home.php');
+}
 
 include_once('header.php');
+
 slack_general('ADMIN: Home Page Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
 ?>
 
@@ -14,7 +23,13 @@ list all users and levels
 list all petitions and stats
 </pre>
 
-
+<?PHP
+$q="SELECT * FROM users";
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+ echo "<li>$d[id] $d[email] $d[name] $d[group_id] $d[sec_level]</li>"; 
+}
+?>
 <pre>
 <?PHP print_r($_COOKIE); ?>
 </pre>
