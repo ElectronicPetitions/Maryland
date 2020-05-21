989fe01d655858f23076dc5a4f8366a9b8c58c47
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 16:13:27 -0400

    Update petitions.php

--- admin/petitions.php
+++ admin/petitions.php
@@ -7,16 +7,17 @@ if ($_COOKIE['level'] == 'user'){
 }
 include_once('header.php');
 slack_general('ADMIN: petition manager Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+$group_id = $_COOKIE['group_id'];
 ?>
 
-<h1>petition manager</h1>
-
-
-
-<pre>
-<?PHP print_r($_COOKIE); ?>
-</pre>
-
+<h1>Petitions</h1>
+<?PHP
+$q="SELECT * FROM petitions where group_id = '$group_id'";
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+ echo "<li>$d[petition_id] $d[web_short_name] $d[web_color] $d[group_id] $d[petition_name] $d[eligibleVoterListField] $d[eligibleVoterListEquals] $d[eligibleVoterListEnforce]</li>"; 
+}
+?>
 
 <?PHP
 include_once('footer.php');
