e18f6dbc4a545168de4a31aae68de384e7cfc431
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 15:41:47 -0400

    Update manager_home.php

--- admin/manager_home.php
+++ admin/manager_home.php
@@ -1,1 +1,10 @@
-manager_home.php
+include_once('../slack.php');
+include_once('security.php');
+if ($_COOKIE['level'] == 'user'){
+  slack_general('ADMIN: Redirect User Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+  header('Location: user_home.php');
+}
+include_once('header.php');
+slack_general('ADMIN: Manager Home Page Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+
+include_once('footer.php');
