81396116e1a3d2ff949cc96686b92eb17ba27465
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 15:42:21 -0400

    Update user_home.php

--- admin/user_home.php
+++ admin/user_home.php
@@ -1,1 +1,6 @@
-user_home.php
+<?PHP
+include_once('security.php');
+include_once('header.php');
+slack_general('ADMIN: User Home Page Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+
+include_once('footer.php');
