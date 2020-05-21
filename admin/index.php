2f29676b7eab99590e7b9c6aaff7a0740e148b37
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 14:22:33 -0400

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -6,8 +6,13 @@ include_once('header.php');
 slack_general('ADMIN: Home Page Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
 ?>
 
-<h1>admin home</h1>
+<h1>Admin Home</h1>
 
+<pre>
+list all users and levels
+
+list all petitions and stats
+</pre>
 
 
 <pre>
