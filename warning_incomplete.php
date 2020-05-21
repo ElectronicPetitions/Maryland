8e16ad821f515215a30a8ad63462e74d94befc88
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 18:22:37 -0400

    Update warning_incomplete.php

--- warning_incomplete.php
+++ warning_incomplete.php
@@ -1,7 +1,9 @@
-<?PHP include_once('header.php');  
+<?PHP 
+include_once('header.php');  
+slack_general('warning_incomplete.php ('.$_COOKIE['invite'].')','md-petition');
 $qX = "select * from website_text where id = '4'";
- $rX = $petition->query($qX);
- $dX = mysqli_fetch_array($rX);
+$rX = $petition->query($qX);
+$dX = mysqli_fetch_array($rX);
 ?>
 <script>document.title = "MEPS - Warning Incomplete";</script>
   <div class='col-sm-12' style='height:100px; text-align:center;'><h2><?PHP echo $dX['text_title'];?></h2></div>
