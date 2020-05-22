e68da81330c082913b2f9c40034bcffc7f59ffae
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 21:41:54 -0400

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -2,7 +2,7 @@
 
 function id2group($id){
   global $petition;
-  $q = "select name from groups where id = '$id';
+  $q = "select name from groups where id = '$id'";
   $r = $petition->query($q);
   $d = mysqli_fetch_array($r,MYSQLI_ASSOC);
   return $d['name'];
