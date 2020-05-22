b749689377e4409053a0193d2d011d43a30847ec
Patrick McGuire <insidenothing@gmail.com>
2020-05-21 21:33:38 -0400

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -13,6 +13,7 @@
   <?PHP 
   include_once('/var/www/secure.php'); //outside webserver
   include_once('../slack.php');
+  include_once('functions.php');
   ?>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
