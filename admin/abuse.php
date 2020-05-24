5623205d2facd233ad34b0e4065256032c46a8f7
Patrick McGuire <insidenothing@gmail.com>
2020-05-24 08:43:20 -0400

    Create abuse.php

---
+++ admin/abuse.php
@@ -0,0 +1,28 @@
+<?PHP 
+include_once('../slack.php');
+include_once('security.php');
+if ($_COOKIE['level'] == 'user'){
+  slack_general('ADMIN: Redirect User Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+  header('Location: user_home.php');
+}
+if ($_COOKIE['level'] == 'manager'){
+  slack_general('ADMIN: Redirect Manager Home ('.$_COOKIE['name'].') ('.$_COOKIE['level'].')','md-petition');
+  header('Location: manager_home.php');
+}
+include_once('header.php');
+?>
+<h1>Abuses</h1>
+<div>We really need to watch for lazy hackers... ok all hackers... we can flag signatures for human review, we WILL be active.</div>
+<h2>IP Address</h2>
+<?PHP
+$q="SELECT ip_address, COUNT(*) as count FROM signatures group by ip_address";
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+ echo "<li>$d[ip_address] $d[count]</li>"; 
+}
+?>
+
+
+<?PHP
+include_once('footer.php');
+?>
