46ca7ade668ae59f07c0a5816e4a7933a107f1f7
Patrick McGuire <insidenothing@gmail.com>
2020-05-24 09:26:00 -0400

    Update mgp.php

--- invite/mgp.php
+++ invite/mgp.php
@@ -40,7 +40,7 @@ slack_general('MGP Home Page Loaded ('.$_COOKIE['invite'].')','md-petition');
   <div class='col-sm-10' style='text-align:center;'><h1><?PHP echo $d['text_title'];?></h1><h2><?PHP echo $d['text_block'];?></h2></div>
  </div> 
 <div class='row'>
-  <div class='col-sm-10' style='text-align:center;'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='enter_information.php'">SIGN <?PHP echo $d['petition_name'];?></button></div>
+  <div class='col-sm-10' style='text-align:center;'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='../enter_information.php'">SIGN <?PHP echo $d['petition_name'];?></button></div>
  </div> 
   
  <div class='row'>
