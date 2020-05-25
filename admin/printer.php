c72564526ce7d86ee1726272cb7987e8ab755f8c
Patrick McGuire <insidenothing@gmail.com>
2020-05-25 11:04:55 -0400

    Update printer.php

--- admin/printer.php
+++ admin/printer.php
@@ -1,16 +1,6 @@
 <?PHP
-function getPage($url){
-    $curl = curl_init();
-    curl_setopt ($curl, CURLOPT_URL, $url);
-    curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire Petition Printer at md-petition.com /%d.0",rand(4,50)));
-    curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
-    $html = curl_exec ($curl);
-    curl_close ($curl);
-    return $html;
-}
 foreach ($_POST['print'] as $k => $v) {
     $url = "http://md-petition.com/admin/print.php?id=$k";
-    echo getPage($url);
+    echo "<img src='$url' alt='$k' style='page-break-after: always;'>";
 }
 die();
