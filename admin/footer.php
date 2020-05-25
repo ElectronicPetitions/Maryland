7fcd4dfea3bea98632117b5ec5e07c2a379babc9
Patrick McGuire <insidenothing@gmail.com>
2020-05-25 11:59:47 -0400

    Update footer.php

--- admin/footer.php
+++ admin/footer.php
@@ -1,4 +1,15 @@
   <div class='col-sm-12' style='text-align:center;'>&COPY; 2020 Patrick McGuire - baltimorehacker@gmail.com - 443-386-2584 (leave message)</div>
-  <div class='col-sm-12' style='text-align:center;'><small><a target='_Blank' href='https://validator.w3.org/nu/?doc=https://www.mdwestserve.com<?PHP echo $_SERVER['SCRIPT_NAME'];?>'><?PHP echo $_SERVER['SCRIPT_NAME'];?></a></small></div>
+  <script type="text/javascript">
+    window.doorbellOptions = {
+        "id": "10512",
+        "appKey": "4wEnfKuAdj4WlHgKaxLMrAbYn8aKdFhtYVnpbRPkG4AbA7SH7KcgiRvdZyWmwxnw"
+    };
+    (function(w, d, t) {
+        var hasLoaded = false;
+        function l() { if (hasLoaded) { return; } hasLoaded = true; window.doorbellOptions.windowLoaded = true; var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/'+window.doorbellOptions['id']+'?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g); }
+        if (w.attachEvent) { w.attachEvent('onload', l); } else if (w.addEventListener) { w.addEventListener('load', l, false); } else { l(); }
+        if (d.readyState == 'complete') { l(); }
+    }(window, document, 'script'));
+</script>
   </div>
 </body>
