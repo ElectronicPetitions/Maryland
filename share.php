b1c121202cfac5a2d40d1d7a5373f2ba53de8260
Patrick McGuire <insidenothing@gmail.com>
2020-05-25 07:08:28 -0400

    Update share.php

--- share.php
+++ share.php
@@ -48,7 +48,7 @@ $d = mysqli_fetch_array($r);
   echo "<tr>
   <td align='center'><small>$d2[petition_name]<small></td>
   <td><div class=\"fb-share-button\" 
-     data-href=\"http://md-petition.com/index.php?invite=$d2[web_short_name]\" 
+     data-href=\"http://md-petition.com/$link\" 
      data-layout=\"box_count\" data-size=\"large\">
    </div></td>
    <td><input type='text' size='50' value='http://md-petition.com/$link' id='$d2[web_short_name]'><button onclick='myFunction(\"$d2[web_short_name]\")'>Copy Link</button></td>
