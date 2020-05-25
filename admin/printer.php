cdb56a58ade725c40a7137b710b6aef55baa7920
Patrick McGuire <insidenothing@gmail.com>
2020-05-25 11:22:44 -0400

    Update printer.php

--- admin/printer.php
+++ admin/printer.php
@@ -2,8 +2,11 @@
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <?PHP
+if(empty($_COOKIE['name'])){
+  die("Error #".__LINE__);
+}
 foreach ($_POST['print'] as $k => $v) {
-    $url = "http://md-petition.com/admin/print.php?id=$k";
+    $url = "http://md-petition.com/admin/print.php?id=$k&name=".$_COOKIE['name'];
     echo "<img class='img-responsive' src='$url' alt='$k' style='page-break-after: always;'>";
 }
 die();
