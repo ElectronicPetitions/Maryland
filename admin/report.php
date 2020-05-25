80b7255033cad76481c5f2189406d22852f98dd2
Patrick McGuire <insidenothing@gmail.com>
2020-05-25 11:26:02 -0400

    Update report.php

--- admin/report.php
+++ admin/report.php
@@ -23,9 +23,39 @@ $group_id = $_COOKIE['group_id'];
 <input type='submit' value='PRINT'>
 <?PHP
   if($_COOKIE['level'] == 'admin'){
-    $q="SELECT * FROM petitions where admin_status = 'approved'";
+    $q="SELECT * FROM petitions where admin_status = 'approved' and printed_status = ''";
   }else{
-    $q="SELECT * FROM petitions where group_id = '$group_id' and admin_status = 'approved'";
+    $q="SELECT * FROM petitions where group_id = '$group_id' and admin_status = 'approved' and printed_status = ''";
+  }
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+  echo "<h1>$d[petition_name]</h1>";
+  echo "<table border='1' cellpadding='0' cellspacing='5'>";
+  unset($hide);
+  $hide = array();
+  $pID = $d['petition_id'];
+  $q2="SELECT * FROM signatures where petition_id = '$pID' order by id desc";
+  $r2 = $petition->query($q2);
+  while($d2 = mysqli_fetch_array($r2)){
+    if (!in_array($d2['VTRID'], $hide)) {
+      $hide[] = $d2['VTRID'];
+      echo "<tr><td><input type='checkbox' name='print[".$d2[id]."]'></td><td>$d2[ip_address]</td><td>$d2[date_time_signed]</td><td>$d2[signed_name_as]</td><td>$d2[signed_name_as_circulator]</td><td>$d2[contact_phone]</td><td>$d2[signature_status]</td><td>$d2[printed_status]</td></tr>";
+    }
+  }
+  echo '</table>';
+}
+?>
+</form>
+
+<form id='form2' name='form2' method='POST' action='printer.php'>
+<a onclick="javascript:checkAll('form2', true);" href="javascript:void();">check all</a>
+<a onclick="javascript:checkAll('form2', false);" href="javascript:void();">uncheck all</a>
+<input type='submit' value='PRINT'>
+<?PHP
+  if($_COOKIE['level'] == 'admin'){
+    $q="SELECT * FROM petitions where admin_status = 'approved' and printed_status <> ''";
+  }else{
+    $q="SELECT * FROM petitions where group_id = '$group_id' and admin_status = 'approved' and printed_status <> ''";
   }
 $r = $petition->query($q);
 while($d = mysqli_fetch_array($r)){
