d1044b705e996258151346f2a4f3dd6f40c9103d
Patrick McGuire <insidenothing@gmail.com>
2020-05-25 12:44:47 -0400

    Update header.php

--- header.php
+++ header.php
@@ -34,55 +34,15 @@ if ($_GET['debug'] == 'off'){
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <title>MEPS - Maryland Eletronic Petition Software</title>
-<?PHP
-  /*
-  <style>
-* {
-  margin: 0;
-}
-html, body {
-  height: 100%;
-}
-.page-wrap {
-  min-height: 100%;
-  
-  margin-bottom: -100px; 
-}
-.page-wrap:after {
-  content: "";
-  display: block;
-}
-.site-footer, .page-wrap:after {
-  height: 100px; 
-}
-.site-footer {
-  background: lightblue;
+<style>
+input:focus {
+  background-color: lightblue;
 }
 </style>
-*/ ?>
 </head>
 <body>
- 
   <div class="container-fluid">
-    <?PHP /*
-    <ul class="nav nav-pills">
-      <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/index.php'){ echo "class='active'"; } ?> ><a href='index.php'>1: Are You Registered?</a></li>
-      <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/not_a_registered_voter.php'){ echo "class='active'"; } ?> ><a>2: How to Register?</a></li>
-      <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/enter_information.php'){ echo "class='active'"; } ?> ><a>3: Enter Information</a></li>
-      <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/is_the_information_correct.php'){ echo "class='active'"; } ?> ><a>4: Confirm Information</a></li>    
-      <?PHP if ($_COOKIE['invite'] == ''){ ?>
-        <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/eligible.php'){ echo "class='active'"; } ?> ><a>5: Select Petition</a></li>
-        <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/petition.php'){ echo "class='active'"; } ?> ><a>6: Sign Petition</a></li>
-      <?PHP }else{ ?>
-        <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/petition.php'){ echo "class='active'"; } ?> ><a>5: Sign <?PHP echo strtoupper($_COOKIE['invite']);?> Petition</a></li>
-        <li role='presentation' <?PHP if($_SERVER['SCRIPT_NAME'] == '/eligible.php'){ echo "class='active'"; } ?> ><a>6: Select Next Petition</a></li>
-      <?PHP } ?>
-      
-      <?PHP if ($_COOKIE['web_name'] != ''){ ?>
-        <li role='presentation'><a href="/reset.php">RESET</a></li>
-      <?PHP } ?>
-    </ul>
-    */ 
+    <?PHP  
     if ($_COOKIE['invite'] != ''){ 
       $invite = $petition->real_escape_string($_COOKIE['invite']);
       $q = "select petition_name, web_color from petitions where web_short_name = '$invite'";
