73a94d3fed06a68c350ae8719298a4e5b9ac540f
Patrick McGuire <insidenothing@gmail.com>
2020-05-26 12:08:33 -0400

    Update abuse.php

--- admin/abuse.php
+++ admin/abuse.php
@@ -10,13 +10,17 @@ if ($_COOKIE['level'] == 'manager'){
   header('Location: manager_home.php');
 }
 include_once('header.php');
+if (isset($_GET['flag_invalid_signature'])){
+  $id = $_GET['flag_invalid_signature']
+  $petition->query("update signatures set signature_status = 'flag_invalid_signature' where id = '$id' ");
+}
 if (isset($_GET['ip_address'])){ 
   $ip = $_GET['ip_address']; 
   echo "<h1>Review $ip</h1><table>";    
   $q = "SELECT * FROM  signatures where ip_address = '$ip' ";
   $r = $petition->query($q);
   while($d = mysqli_fetch_array($r)){
-    echo "<tr><td><b>$d[date_time_signed]</b></td><td><a href='?VTRID=$d[VTRID]'>$d[VTRID]</a></td><td>$d[petition_id]</td><td>$d[signed_name_as]</td><td>$d[signed_name_as_circulator]</td><td>$d[contact_phone]</td><td>$d[signature_status]</td></tr>"; 
+    echo "<tr><td><b>$d[date_time_signed]</b></td><td><a href='?VTRID=$d[VTRID]'>$d[VTRID]</a></td><td>$d[petition_id]</td><td>$d[signed_name_as]</td><td>$d[signed_name_as_circulator]</td><td>$d[contact_phone]</td><td>$d[signature_status]</td><td>$d[printed_status]</tr>"; 
   }
   echo "</table>";
 }elseif (isset($_GET['VTRID'])){ 
@@ -25,7 +29,7 @@ if (isset($_GET['ip_address'])){
   $q = "SELECT * FROM  signatures where VTRID = '$VTRID' ";
   $r = $petition->query($q);
   while($d = mysqli_fetch_array($r)){
-    echo "<tr><td><b>$d[date_time_signed]</b></td><td><a href='?ip_address=$d[ip_address]'>$d[ip_address]</a></td><td>$d[petition_id]</td><td>$d[signed_name_as]</td><td>$d[signed_name_as_circulator]</td><td>$d[contact_phone]</td><td>$d[signature__status]</td></tr>"; 
+    echo "<tr><td><b>$d[date_time_signed]</b></td><td><a href='?ip_address=$d[ip_address]'>$d[ip_address]</a></td><td>$d[petition_id]</td><td>$d[signed_name_as]</td><td>$d[signed_name_as_circulator]</td><td>$d[contact_phone]</td><td>$d[signature_status]</td><td>$d[printed_status]</td><td><a href='?flag_invalid_signature=$d[id]'>flag_invalid_signature</a></td></tr>"; 
   }
   echo "</table>";
 }
