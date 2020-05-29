5d2c078623c3bcd56376c9c784126c8d2a0f346e
Patrick McGuire <insidenothing@gmail.com>
2020-05-29 11:14:11 -0400

    Update printer.php

--- admin/printer.php
+++ admin/printer.php
@@ -5,8 +5,20 @@
 if(empty($_COOKIE['name'])){
   die("Error #".__LINE__);
 }
+include_once('/var/www/secure.php'); //outside webserver
 foreach ($_POST['print'] as $k => $v) {
     $url = "http://md-petition.com/admin/print.php?id=$k&name=".$_COOKIE['name'];
     echo "<img class='img-responsive' src='$url' alt='$k' style='page-break-after: always;'>";
+    $q = "select petition_id from signatures where id = '$id'";
+    $r = $petition->query($q);
+    $d = mysqli_fetch_array($r);
+    $q = "select petition_jpg_page2 from petitions where petition_id = '$d[petition_id]'";
+    $r = $petition->query($q);
+    $d = mysqli_fetch_array($r);
+    if ($d['petition_jpg_page2'] != ''){ ?>
+    <div class="row">
+      <div class='col-sm-12'><img class="img-responsive" src='../<?PHP echo $d['petition_jpg_page2'];?>'></div>
+    </div>
+    <?PHP }
 }
 die();
