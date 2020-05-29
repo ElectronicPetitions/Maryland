9c7e1c23803f9e0220b98a69d32e6355396cffce
Patrick McGuire <insidenothing@gmail.com>
2020-05-29 11:01:47 -0400

    Create printer.php

---
+++ printer.php
@@ -0,0 +1,20 @@
+<?PHP
+$id = intval($_GET['id']);
+include_once('/var/www/secure.php'); //outside webserver
+$q = "select * from petitions where petition_id = '$id'";
+$r = $petition->query($q);
+$d = mysqli_fetch_array($r);
+?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<div class="container">
+  <div class="row">
+    <div class='col-sm-12'><img class="img-responsive" src='https://www.md-petition.com/soft_copy.php?id=140'></div>
+  </div>
+  <?PHP if ($d['petition_jpg_page2'] != ''){ ?>
+    <div class="row">
+      <div class='col-sm-12'><img class="img-responsive" src='<?PHP echo $d['petition_jpg_page2'];?>'></div>
+    </div>
+  <?PHP } ?>
+</div>
