1c2eead98787c1409636e585c5aadc6f16b0facc
Patrick McGuire <insidenothing@gmail.com>
2020-06-01 10:33:20 -0400

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -2,7 +2,7 @@
 
 function id2petition($id){
   global $petition;
-  $q = "select petition_name from petition where petition_id = '$id'";
+  $q = "select petition_name from petitions where petition_id = '$id'";
   $r = $petition->query($q);
   $d = mysqli_fetch_array($r,MYSQLI_ASSOC);
   return $d['petition_name'];
