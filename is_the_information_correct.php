65433c5533a3cecfbd03ced5d72fb793a41a0ecf
Patrick McGuire <insidenothing@gmail.com>
2020-06-01 09:58:14 -0400

    Update is_the_information_correct.php

--- is_the_information_correct.php
+++ is_the_information_correct.php
@@ -48,7 +48,7 @@ $qX = "select * from website_text where id = '6'";
 ?>
 <script>document.title = "MEPS - Confirm information";</script>
 <div class='row'>
-    <div class='col-sm-10' style='height:100px; text-align:center;'><h1><?PHP echo $dX['text_title'];?></h1><h2><?PHP echo $dX['text_block'];?></h2></div>
+    <div class='col-sm-10' style='text-align:center;'><h1><?PHP echo $dX['text_title'];?></h1><h2><?PHP echo $dX['text_block'];?></h2></div>
 </div>
 <div class='row'>
   <div class='col-sm-10'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='eligible.php'"><img class='click_me' src="files/click_here.gif">YES</button></div>
