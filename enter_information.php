0909e02f9191e6863c4f93ace31787b1516e3c24
Patrick McGuire <insidenothing@gmail.com>
2020-06-01 12:55:07 -0400

    Update enter_information.php

--- enter_information.php
+++ enter_information.php
@@ -62,7 +62,9 @@ slack_general('Enter Information ('.$_COOKIE['invite'].')','md-petition');
 ?>
 <script>document.title = "MEPS - Enter Information";</script>
 <form method='POST'>
-  
+    <div class='row'>
+     <div class='col-sm-10' style='text-align:center;'><button type="submit" class="btn btn-success btn-lg btn-block"><img class='click_me' src="files/click_here.gif">Next</button></div>
+  </div>
   <div class='row'>
     <div class='col-sm-10' style='text-align:center;'><h1><?PHP echo $dX['text_title'];?></h1><h2 style="margin:25px; padding25px; background-color:lightyellow;"><?PHP echo $dX['text_block'];?></h2></div>
   </div>
@@ -123,11 +125,10 @@ slack_general('Enter Information ('.$_COOKIE['invite'].')','md-petition');
      <div class='col-sm-7' style='text-align:left; background-color:lightyellow;'><h3>55555</h3></div>
   </div> 
    
-  <div class='row'>
-    <div class='col-sm-5' style='text-align:center;'><button type="reset" class="btn btn-warning btn-lg btn-block not_me">Clear</button></div>
-    <div class='col-sm-5' style='text-align:center;'><button type="submit" class="btn btn-success btn-lg btn-block"><img class='click_me' src="files/click_here.gif">Next</button></div>
-  </div>
-   
+
+   <div class='row'>
+    <div class='col-sm-10' style='text-align:center;'><button type="reset" class="btn btn-warning btn-lg btn-block not_me">Clear</button></div>
+   </div>
 </form>
 
 <?PHP include_once('footer.php');
