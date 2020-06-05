d5f7c1e842f99173e539f8ff61df564b1b89b91c
Patrick McGuire <insidenothing@gmail.com>
2020-06-05 10:41:25 -0400

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -19,7 +19,7 @@ slack_general('ADMIN: Home Page Loaded ('.$_COOKIE['name'].') ('.$_COOKIE['level
 ?>
 
 <h1>Admin Home</h1>
-
+<div id="chartContainer1" style="height: 400px; width: 100%; margin: 0px auto;"></div>
 <h1>Users</h1>
 <?PHP
 $q="SELECT * FROM users";
@@ -74,7 +74,92 @@ while($d = mysqli_fetch_array($r)){
 }
 ?>
 
+<?PHP
+	$pID = 1;
+	//echo "<div id=\"chartContainer$pID\" style=\"height: 400px; width: 100%; margin: 0px auto;\"></div>";
+	$chart='';
+	$chart2='';
+	$chart3='';
+	$q3 = "SELECT just_date FROM signatures where just_date <> '0000-00-00' group by just_date";
+	//echo "<li>$q3</li>";
+	$r3 = $petition->query($q3);
+	$total=0;
+	$goal = $d['signature_goal'];
+	if ($goal == 0){
+		$goal = 10000;
+	}
+	while ($d3 = mysqli_fetch_array($r3)){
+	  $just_date = $d3['just_date'];
+	  $q2 = "SELECT * FROM signatures where just_date = '$just_date' and signature_status = 'verified'  ";
+	  //echo "<li>$q2</li>";
+	  $r2 = $petition->query($q2);
+	  $count  = mysqli_num_rows($r2);
+	  $chart .=  '{ label: "'.$just_date.'", y: '.intval($count).' }, ';
+	  $total = $total + intval($count);
+	  $chart2 .=  '{ label: "'.$just_date.'", y: '.intval($total).' }, ';
+	  $goal = $goal - intval($count);
+	  $chart3 .=  '{ label: "'.$just_date.'", y: '.intval($goal).' }, ';
+	}
+	$chart = rtrim(trim($chart), ",");
+	$chart2 = rtrim(trim($chart2), ",");
+	$chart3 = rtrim(trim($chart3), ",");
+	
+	ob_start(); ?>
+
+	var chart<?PHP echo $pID;?> = new CanvasJS.Chart("chartContainer<?PHP echo $pID;?>", {
+		theme:"light2",
+		animationEnabled: true,
+		exportEnabled: true,
+		title:{
+			text: "MD-Petition.com Signature Tracker"
+		},
+		axisY :{
+			includeZero: false,
+			title: "Number of Signatures",
+			suffix: "",
+	    scaleBreaks: {
+					autoCalculate: true
+				}
+		},
+		toolTip: {
+			shared: "true"
+		},
+		legend:{
+			cursor:"pointer",
+			itemclick : toggleDataSeries
+		},
+		data: [{
+			type: "line",
+			visible: true,
+			showInLegend: true,
+			yValueFormatString: "#####",
+			name: "Total Signatures Count",
+			dataPoints: [
+				<?PHP echo $chart2; ?>
+			]
+		},{
+			type: "column",
+			visible: true,
+			showInLegend: true,
+			yValueFormatString: "#####",
+			name: "New Daily Signatures",
+			dataPoints: [
+				<?PHP echo $chart; ?>
+			]
+		}]
+	}
+
+
+				      );
+	chart<?PHP echo $pID;?>.render();
+
+	<?PHP $javascript .= ob_get_clean(); ?>
+
+
+
 
 <?PHP
+echo $javascript;
+
 include_once('footer.php');
 ?>
