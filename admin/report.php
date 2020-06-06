5a5d6cfb50ca8ca7a39f68f756bec788bf2aa73e
Patrick McGuire <insidenothing@gmail.com>
2020-06-06 11:27:12 -0400

    sep out flagged

--- admin/report.php
+++ admin/report.php
@@ -171,7 +171,7 @@ while($d = mysqli_fetch_array($r)){
   unset($hide);
   $hide = array();
   $pID = $d['petition_id'];
-  $q2="SELECT * FROM signatures where petition_id = '$pID' and printed_status = '' and signature_status <> 'deleted' order by signature_status, id desc";
+  $q2="SELECT * FROM signatures where petition_id = '$pID' and printed_status = '' and signature_status = 'verified' order by signature_status, id desc";
   $r2 = $petition->query($q2);
   while($d2 = mysqli_fetch_array($r2)){
     if ($d2['signature_status'] == 'verified'){
@@ -189,6 +189,48 @@ while($d = mysqli_fetch_array($r)){
 }
     ?>
 </form>
+
+
+
+
+<form id='form5' name='form5' method='POST' action='printer.php'>
+<?PHP
+  if($_COOKIE['level'] == 'admin'){
+    $q="SELECT * FROM petitions where admin_status = 'approved' ";
+  }else{
+    $q="SELECT * FROM petitions where group_id = '$group_id' and admin_status = 'approved'";
+  }
+$r = $petition->query($q);
+while($d = mysqli_fetch_array($r)){
+  echo "<fieldset style='background-color:$d[web_color];'><legend style='background-color:white;'>$d[petition_name] - Flagged for Deletion or Override</legend>
+  <a onclick=\"javascript:checkAll('form3', true);\" href=\"javascript:void();\">Check All</a>
+  <a onclick=\"javascript:checkAll('form3', false);\" href=\"javascript:void();\">Uncheck All</a>
+  <input type='submit' value='PRINT'>";
+  echo "<table border='1' cellpadding='0' cellspacing='5'>";
+  unset($hide);
+  $hide = array();
+  $pID = $d['petition_id'];
+  $q2="SELECT * FROM signatures where petition_id = '$pID' and printed_status = '' and signature_status <> 'deleted' and signature_status <> 'verified' order by signature_status, id desc";
+  $r2 = $petition->query($q2);
+  while($d2 = mysqli_fetch_array($r2)){
+    if ($d2['signature_status'] == 'verified'){
+      echo "<tr>
+      <td><input type='checkbox' name='print[".$d2[id]."]'></td><td>$d2[ip_address]</td><td>$d2[date_time_signed]</td><td>$d2[signed_name_as]</td><td>$d2[signed_name_as_circulator]</td><td>$d2[contact_phone]</td><td>$d2[signature_status]-<a href='?review=$d2[id]'>Flag for Review</a></td><td>$d2[printed_status]</td></tr>";
+    }else{
+      echo "<tr>
+      <td><a href='?override=$d2[id]'>Override</a> or <a href='?delete=$d2[id]'>Delete</a></td><td>$d2[ip_address]</td><td>$d2[date_time_signed]</td><td>$d2[signed_name_as]</td><td>$d2[signed_name_as_circulator]</td><td>$d2[contact_phone]</td><td>$d2[signature_status]</td><td>$d2[printed_status]</td></tr>";
+    }
+  }
+  echo '</table></fieldset>';
+
+	
+	
+}
+    ?>
+</form>
+
+
+
 
 <form id='form2' name='form2' method='POST' action='printer.php'>
 
