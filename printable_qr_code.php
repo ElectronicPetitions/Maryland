08d08f2918ed4e088cea1a99c744efb5d92fd5fe
Patrick McGuire <insidenothing@gmail.com>
2020-06-08 10:40:51 -0400

    Create printable_qr_code.php

---
+++ printable_qr_code.php
@@ -0,0 +1,36 @@
+<html lang="en">  
+<head>
+  <meta property="og:url"           content="https://www.md-petition.com/printable_qr_code.php" />
+  <meta property="og:type"          content="website" />
+  <meta property="og:title"         content="Maryland Electronic Petition Software" />
+  <meta property="og:description"   content="Socially Distant Petitions" />
+  <meta property="og:image"         content="http://md-petition.com/files/maryland-flag-graphic.png" />
+ 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165887820-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-165887820-1');
+  </script>
+  <?PHP 
+  include_once('/var/www/secure.php'); //outside webserver
+  include_once('slack.php');
+  ?>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <title>Printable QR Code Invite</title>
+<?PHP
+$short = $_GET['short'];
+$q2 = "SELECT * FROM petitions where web_short_name = '$short'";
+$r2 = $petition->query($q2);
+$d2 = mysqli_fetch_array($r2);
+$link = "?invite=$d2[web_short_name]";
+if ($d2['landing_page'] != ''){
+  $link = $d2['landing_page']; 
+}
+echo "<h1>$d2[petition_name]</h1><br><img class='img-responsive' src='https://www.md-petition.com/qrcode.php?s=qrl&d=https://www.md-petition.com/$link'>";
+?>
