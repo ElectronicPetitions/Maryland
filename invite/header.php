2411288c0611e8ae6449fc7bc6e1421a57c180d6
Patrick McGuire <insidenothing@gmail.com>
2020-06-09 10:11:24 -0400

    Update header.php

--- invite/header.php
+++ invite/header.php
@@ -1,8 +1,2 @@
 <?PHP
 include_once('../session.php');
-function presign(){
-  global $petition;
-  $id     = session_id();
-  $page   = $_SERVER['REQUEST_URI'];
-  $petition->query("insert into presign (php_session_id,php_page) values ('$id','$page') "); 
-}
