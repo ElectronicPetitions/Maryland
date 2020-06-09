52d1ff5337da56d2ce4791f49ac9c4b037787bf6
Patrick McGuire <insidenothing@gmail.com>
2020-06-09 11:36:36 -0400

    Update session.php

--- session.php
+++ session.php
@@ -15,10 +15,11 @@ function presign(){
 	} else {
 	    	$ip = $_SERVER['REMOTE_ADDR'];
 	}
+  $petition_name   = $petition->real_escape_string($_COOKIE['invite']).'-'.$petition->real_escape_string($_COOKIE['pID']);
   $ip = $petition->real_escape_string($ip);
   $browser_string = $petition->real_escape_string($_SERVER['HTTP_USER_AGENT']);
   $now = $petition->real_escape_string(date('r'));
-  $petition->query("insert into presign (php_session_id,php_page,name,email_for_follow_up,phone_for_validation,ip_address,browser_string,action_on) 
-  values ('$id','$page','$name','$email','$phone','$ip','$browser_string','$now') "); 
+  $petition->query("insert into presign (petition,php_session_id,php_page,name,email_for_follow_up,phone_for_validation,ip_address,browser_string,action_on) 
+  values ('$petition_name','$id','$page','$name','$email','$phone','$ip','$browser_string','$now') "); 
 }
 ?>
