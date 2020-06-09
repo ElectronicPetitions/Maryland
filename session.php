9dc52bb0e845a85f44994ddfef790874a28117ee
Patrick McGuire <insidenothing@gmail.com>
2020-06-09 10:05:12 -0400

    Update session.php

--- session.php
+++ session.php
@@ -1,4 +1,10 @@
 <?PHP
 session_start();
 // this will overlay our current system
+function presign(){
+  global $petition;
+  $id     = session_id();
+  $page   = $_SERVER['REQUEST_URI'];
+  $petition->query("insert into presign (php_session_id,php_page) values ('$id','$page') "); 
+}
 ?>
