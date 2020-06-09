f655676f0e570b7061cbc1b60932a54237c1c48d
Patrick McGuire <insidenothing@gmail.com>
2020-06-09 10:43:51 -0400

    Update session.php

--- session.php
+++ session.php
@@ -5,6 +5,10 @@ function presign(){
   global $petition;
   $id     = session_id();
   $page   = $_SERVER['REQUEST_URI'];
-  $petition->query("insert into presign (php_session_id,php_page) values ('$id','$page') "); 
+  $email  = $_COOKIE['email'];
+  $name   = $_COOKIE['pNAME'];
+  $phone  = $_COOKIE['pPHONE'];
+  $petition->query("insert into presign (php_session_id,php_page,name,email_for_follow_up,phone_for_validation) 
+  values ('$id','$page','$name','$email','$phone') "); 
 }
 ?>
