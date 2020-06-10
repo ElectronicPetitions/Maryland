93e9e8d80de336d93bc5df146fc0f1b39a0fcb17
Patrick McGuire <insidenothing@gmail.com>
2020-06-09 20:42:36 -0400

    Update is_the_information_correct.php

--- is_the_information_correct.php
+++ is_the_information_correct.php
@@ -62,7 +62,7 @@ $qX = "select * from website_text where id = '6'";
     <div class='col-sm-10' style='text-align:center;'><h1><?PHP echo $dX['text_title'];?></h1><h2><?PHP echo $dX['text_block'];?></h2></div>
 </div>
 <div class='row'>
-  <div class='col-sm-10'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='eligible.php'"><img class='click_me' src="files/click_here.gif">YES</button></div>
+  <div class='col-sm-10'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='eligible.php'"><img alt='Click Here to Continue' class='click_me' src="files/click_here.gif">YES</button></div>
 </div>
 <div class='row'>
   <div class='col-sm-5' style='text-align:right;'><h2>First Name</h2></div>
@@ -101,7 +101,7 @@ $qX = "select * from website_text where id = '6'";
   <div class='col-sm-5' style='text-align:left;'><h2><?PHP echo $RESIDENTIALZIP5;?></h2></div>
 </div>
 <div class='row'>
-  <div class='col-sm-10'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='eligible.php'"><img class='click_me' src="files/click_here.gif">YES</button></div>
+  <div class='col-sm-10'><button type="button" class="btn btn-success btn-lg btn-block" onclick="window.location.href='eligible.php'"><img alt='Click Here to Continue' class='click_me' src="files/click_here.gif">YES</button></div>
 </div>
 <div class='row'>
   <div class='col-sm-10'><button type="button" class="btn btn-danger btn-lg btn-block not_me" onclick="window.location.href='reset.php'">NO</button></div>
