0a7b36edcc98090eabb7d37754188f91876240e5
Patrick McGuire <insidenothing@gmail.com>
2020-06-11 10:24:42 -0400

    Update menu_admin.php

--- admin/menu_admin.php
+++ admin/menu_admin.php
@@ -1,4 +1,4 @@
-<li role='presentation' style='background-color:red;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/analytics.php'){ echo "class='active'"; } ?> ><a href="abuse.php">Signature Analytics</a></li>
+<li role='presentation' style='background-color:orange;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/analytics.php'){ echo "class='active'"; } ?> ><a href="analytics.php">Signature Analytics</a></li>
 <li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/website.php'){ echo "class='active'"; } ?> ><a href="website.php">Website Text</a></li>
 <li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/admins.php'){ echo "class='active'"; } ?> ><a href="admins.php">Admins</a></li>
 <li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/groups.php'){ echo "class='active'"; } ?> ><a href="groups.php">Groups</a></li>
