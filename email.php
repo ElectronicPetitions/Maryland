46790afe7703860c2062942e4a7c147ba5589c00
Patrick McGuire <insidenothing@gmail.com>
2020-06-12 22:26:59 -0400

    Update email.php

--- email.php
+++ email.php
@@ -1,35 +1,33 @@
 <?PHP
+// Pear Mail Library
 require_once "Mail.php";
 include_once('/var/www/secure.php');
-
-
-$from = "MD-Petition.com Support <mdpetition@gmail.com>";
-$to = "Patrick <insidenothing@gmail.com>";
-$subject = "Hi!";
-$body = "Hi,\n\nHow are you?";
-
-$host = "smtp.gmail.com";
-$port = "587";
 global $gmail_email_user;
-$username = $gmail_email_user;
 global $gmail_email_pass;
-$password = $gmail_email_pass;
 
-$headers = array ('From' => $from,
-  'To' => $to,
-  'Subject' => $subject);
-$smtp = Mail::factory('smtp',
-  array ('host' => $host,
-    'port' => $port,
-    'auth' => true,
-    'username' => $username,
-    'password' => $password));
+$from = 'MD-Petition.com Support <mdpetition@gmail.com>';
+$to = '<insidenothing@gmail.com>';
+$subject = 'Hi!';
+$body = "Hi,\n\nHow <b>are</b> you?";
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => $gmail_email_user,
+        'password' => $gmail_email_pass
+    ));
 
 $mail = $smtp->send($to, $headers, $body);
 
 if (PEAR::isError($mail)) {
-  echo("<p>" . $mail->getMessage() . "</p>");
- } else {
-  echo("<p>Message successfully sent!</p>");
- }
- ?>
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}
