81b5c6b2e88c100952f4f585097bcbd486969f13
Patrick McGuire <insidenothing@gmail.com>
2020-06-13 10:23:18 -0400

    Update LPMD.php

--- invite/LPMD.php
+++ invite/LPMD.php
@@ -42,8 +42,8 @@ slack_general('MLP Home Page Loaded ('.$_COOKIE['invite'].')','md-petition');
   <div class='col-sm-10' style='text-align:center;'><h1><?PHP echo $d['text_title'];?></h1><h2 style='text-align:left;'><?PHP echo $d['text_block'];?></h2></div>
 </div> 
   <div class='row'>
-    <div class='col-sm-3' style='text-align:center;'><img class="img-responsive" alt="<?PHP echo $d['text_title'];?>" src="<?PHP echo $d['logo_url'];?>"></div>
-    <div class='col-sm-7' style='text-align:center;'><h3>If you need assistance, please contact:</h3></div>
+    <div class='col-sm-5' style='text-align:center;'><h3>If you need assistance, please contact:</h3></div>
+    <div class='col-sm-5' style='text-align:center;'><img class="img-responsive" alt="<?PHP echo $d['text_title'];?>" src="<?PHP echo $d['logo_url'];?>"></div>
   </div> 
   <style>
     .social { font-size: large; font-weight:bold; }
