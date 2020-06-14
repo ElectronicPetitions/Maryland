a2f9d047f7ccefebf83c21235371e07bb97efdb2
Patrick McGuire <insidenothing@gmail.com>
2020-06-14 10:34:01 -0400

    Update download.php

--- admin/download.php
+++ admin/download.php
@@ -1,1 +1,52 @@
-on the fly pdf generation - batch printing
+<?PHP
+include_once('/var/www/secure.php'); 
+function mysqli_field_name($result, $field_offset)
+{
+    $properties = mysqli_fetch_field_direct($result, $field_offset);
+    return is_object($properties) ? $properties->name : null;
+}
+if ($_COOKIE['level'] == 'admin'){
+  $q = "SELECT date_time_signed, signed_name_as, printed_status FROM petitions where signature_status = 'verified' ORDER BY id";
+}else{
+  $q = "SELECT date_time_signed, signed_name_as, printed_status FROM petitions where petition_id = '$_COOKIE[petition_id]' and signature_status = 'verified' ORDER BY id";
+}
+
+$r = $core->query($q);
+
+$fields = mysqli_num_fields ( $r );
+
+for ( $i = 0; $i < $fields; $i++ )
+{
+    $header .= mysqli_field_name( $r , $i ) . "\t";
+}
+// https://www.php.net/manual/en/mysqli-result.fetch-row.php
+while( $row = $r->fetch_row() )
+{
+    $line = '';
+    foreach( $row as $value )
+    {                                            
+        if ( ( !isset( $value ) ) || ( $value == "" ) )
+        {
+            $value = "\t";
+        }
+        else
+        {
+            $value = str_replace( '"' , '""' , $value );
+            $value = '"' . $value . '"' . "\t";
+        }
+        $line .= $value;
+    }
+    $data .= trim( $line ) . "\n";
+}
+$data = str_replace( "\r" , "" , $data );
+
+if ( $data == "" )
+{
+    $data = "\n(0) Records Found!\n";                        
+}
+
+header("Content-type: application/octet-stream");
+header("Content-Disposition: attachment; filename=petition_signatures_".time().".xls");
+header("Pragma: no-cache");
+header("Expires: 0");
+print "$header\n$data";
