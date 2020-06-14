7f022e7536e3c1687c5038ae384ffb17aba40eab
Patrick McGuire <insidenothing@gmail.com>
2020-06-14 10:46:46 -0400

    Update menu_user.php

--- admin/menu_user.php
+++ admin/menu_user.php
@@ -1,3 +1,3 @@
 <li role='presentation' style='background-color:lightyellow;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/report.php'){ echo "class='active'"; } ?> ><a href="report.php">Reports</a></li>
-<li role='presentation' style='background-color:lightyellow;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/download.php'){ echo "class='active'"; } ?> ><a href="download.php">Download CSV</a></li>
+<li role='presentation' style='background-color:lightyellow;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/download.php'){ echo "class='active'"; } ?> ><a href="download.php">Download</a></li>
       
