6f2925c27cd897a146e1ff8a5d62fa3401b7f57e
Patrick McGuire <insidenothing@gmail.com>
2020-06-19 12:40:20 -0400

    Update menu_admin.php

--- admin/menu_admin.php
+++ admin/menu_admin.php
@@ -1,5 +1,6 @@
 <li role='presentation' style='background-color:orange;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/analytics.php'){ echo "class='active'"; } ?> ><a href="analytics.php">Analytics</a></li>
-<li role='presentation' style='background-color:orange;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/follow_up_emails.php'){ echo "class='active'"; } ?> ><a href="follow_up_emails.php">Send Follow Up</a></li>
+<li role='presentation' style='background-color:orange;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/follow_up_emails.php'){ echo "class='active'"; } ?> ><a href="follow_up_emails.php">Send</a></li>
+<li role='presentation' style='background-color:orange;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/follow_up_success.php'){ echo "class='active'"; } ?> ><a href="follow_up_success.php">Status</a></li>
 <li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/website.php'){ echo "class='active'"; } ?> ><a href="website.php">Website Text</a></li>
 <li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/admins.php'){ echo "class='active'"; } ?> ><a href="admins.php">Admins</a></li>
 <li role='presentation' style='background-color:lightblue;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/managers.php'){ echo "class='active'"; } ?> ><a href="managers.php">Managers</a></li>          
