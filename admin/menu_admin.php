39cb7de2eb69b8be745b51edd08253e143dacd48
Patrick McGuire <insidenothing@gmail.com>
2020-06-23 12:27:53 -0400

    Update menu_admin.php

--- admin/menu_admin.php
+++ admin/menu_admin.php
@@ -1,3 +1,10 @@
 <li role='presentation' style='background-color:orange;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/analytics.php'){ echo "class='active'"; } ?> ><a href="analytics.php">Analytics</a></li>
+<?PHP
+$q="SELECT id FROM follow_up where status = 'NEW'";
+$r = $petition->query($q);
+$d = mysqli_fetch_array($r);
+if (intval($d['id']) > 0){
+?>
 <li role='presentation' style='background-color:orange;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/follow_up_emails.php'){ echo "class='active'"; } ?> ><a href="follow_up_emails.php">Send Follow Up</a></li>
+<?PHP } ?>
 <li role='presentation' style='background-color:orange;' <?PHP if($_SERVER['SCRIPT_NAME'] == '/admin/follow_up_success.php'){ echo "class='active'"; } ?> ><a href="follow_up_success.php">Follow Up Status</a></li>
