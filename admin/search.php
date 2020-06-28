f890236adf1d868a3fd7a63f384c88341a9cacc6
Patrick McGuire <insidenothing@gmail.com>
2020-06-28 19:52:12 -0400

    Update search.php

--- admin/search.php
+++ admin/search.php
@@ -13,10 +13,13 @@ $LASTNAME='';
 if ($_POST['LASTNAME']){
   $LASTNAME=$_POST['LASTNAME'];
 }
-$q = "select * from VoterList where FIRSTNAME = '$FIRSTNAME' and LASTNAME = '$LASTNAME' ";
-$r = $petition->query($q);
-while ($d = mysqli_fetch_array($r,MYSQL_ASSOC)){
-echo "<li>House Number: $d[HOUSE_NUMBER] Zip Code: $d[RESIDENTIALZIP5]</li>";
+if($FIRSTNAME != '' && $LASTNAME != ''){
+  $q = "select * from VoterList where FIRSTNAME = '$FIRSTNAME' and LASTNAME = '$LASTNAME' ";
+  echo "$q";
+  $r = $petition->query($q);
+  while ($d = mysqli_fetch_array($r,MYSQL_ASSOC)){
+  echo "<li>House Number: $d[HOUSE_NUMBER] Zip Code: $d[RESIDENTIALZIP5]</li>";
+  }
 }
 ?>
 <form method='POST'>
