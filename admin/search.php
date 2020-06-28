9eb137133d3d2e89b06a7ffe34021780cfe0e1fc
Patrick McGuire <insidenothing@gmail.com>
2020-06-28 19:46:09 -0400

    Create search.php

---
+++ admin/search.php
@@ -0,0 +1,29 @@
+<?PHP 
+include_once('bots.php');
+include_once('../slack.php');
+include_once('security.php');
+include_once('/var/www/secure.php'); //outside webserver
+include_once('functions.php');
+include_once('header.php');
+$FIRSTNAME='';
+if ($_POST['FIRSTNAME']){
+  $FIRSTNAME=$_POST['FIRSTNAME'];
+}
+$LASTNAME='';
+if ($_POST['LASTNAME']){
+  $LASTNAME=$_POST['LASTNAME'];
+}
+$q = "select * from VoterList where FIRSTNAME = '$FIRSTNAME' and LASTNAME = '$LASTNAME' ";
+$r = $petition->query($q);
+while ($d = mysqli_fetch_array($r,MYSQL_ASSOC)){
+echo "<li>House Number: $d[HOUSE_NUMBER] Zip Code: $d[RESIDENTIALZIP5]</li>";
+}
+?>
+<form method='POST'>
+<input name='FIRSTNAME'><input name='LASTNAME'><input type='submit'>
+</form>
+
+<?
+
+
+include_once('footer.php');
