9b59323205fd60ed8df35c290c68f9fbc080fa39
Patrick McGuire <insidenothing@gmail.com>
2020-07-03 10:45:20 -0400

    Update analytics.php

--- admin/analytics.php
+++ admin/analytics.php
@@ -26,7 +26,7 @@ function js_redirect($page){ // now header - prep for full auto
   if ($pos === false) {
     // email not found - good to redirect
     //echo "<script>window.location.href = \"$url\";</script>";
-    slack_general($url,'automation');
+    slack_general('js_redirect('.$page.')','automation');
     header('Location: '.$url);
     //slack_general('CHECK COOKIE ('.$_COOKIE['sign_email'].') PAGE ('.$page.')','md-petition-admin');
     die(); 
