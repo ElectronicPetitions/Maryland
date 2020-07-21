9f5b3b3862dfa5bbcb2ad8cf6e92e6e8e9516222
Patrick McGuire <insidenothing@gmail.com>
2020-07-21 15:23:15 -0400

    Update maryland_voter.php

--- api/maryland_voter.php
+++ api/maryland_voter.php
@@ -3,55 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function cut_part_out($start,$end,$whole){
-  $parts = explode($start,$whole);
-  $subparts = explode($end,$parts[1]);
-  $out = $subparts[0];
-  return $out;
-}
-
-function getPage($url,$cookie,$post){
-  $url = str_replace('[month]',date('F'),$url); // replace month January through December
-  $url = str_replace('[day]',date('j'),$url); // replace day 1 to 31
-  $url = str_replace('[yesterday]',date('j',strtotime('yesterday')),$url); // replace day 1 to 31
-  $url = str_replace('[year]',date('Y'),$url); // replace year Examples: 1999 or 2003
-  $curl = curl_init();
-  curl_setopt ($curl, CURLOPT_URL, $url);
-  curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire MEPS https://www.md-petition.com/ /%d.0",rand(4,50)));
-  curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt ($curl, CURLOPT_HEADER, 1);
-  curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
-  curl_setopt ($curl, CURLOPT_FOLLOWLOCATION, true);
-  if ($cookie != ''){
-    curl_setopt ($curl, CURLOPT_HTTPHEADER, array("Cookie: ASP.NET_SessionId=$cookie")); // use cookies 
-  }
-  /* 
-  $postfields = array(
-    'upload_file' => '@file_to_upload.png',
-    'upload_text' => '@text_to_upload'
-  );
-  */
-  if ($post != ''){
-    curl_setopt ($curl, CURLOPT_POSTFIELDS, $post);
-  }
-  $html = curl_exec ($curl);
-  curl_close ($curl);
-  return $html;
-}
-
-// start a session to get a cookie
-$form['url']  = 'https://voterservices.elections.maryland.gov/VoterSearch';
-$form['html'] = getPage($form['url'],'','');
-
-// extract the cookie from the header (see CURLOPT_HEADER)
-$cookie = cut_part_out('ASP.NET_SessionId=',';',$form['html']);
-// extract the form elements we will need to post with our data
-$post['__VIEWSTATE'] = cut_part_out('id="__VIEWSTATE" value="','"',$form['html']);
-$post['__VIEWSTATEGENERATOR'] = cut_part_out('id="__VIEWSTATEGENERATOR" value="','"',$form['html']);
-$post['__VIEWSTATEENCRYPTED'] = cut_part_out('id="__VIEWSTATEENCRYPTED" value="','"',$form['html']);
-$post['__EVENTVALIDATION'] = cut_part_out('id="__EVENTVALIDATION" value="','"',$form['html']);
-// Voter Info Form Elements
-//$post['Languages_field'] = 'ctl00$MainContent$listLanguages';
 $post['ctl00$MainContent$listLanguages'] = "en";
 $post['listLanguages'] = "en";
 if (isset($_POST['SearchFirstName'])){
@@ -118,6 +69,94 @@ if (isset($_POST['SearchMiddleInitial'])){
   $post['ctl00$MainContent$txtSearchMiddleInitial'] = '';
   $post['txtSearchMiddleInitial'] = '';
 }
+
+
+?>
+
+<form method='POST'>
+<table>
+  <tr>
+    <td>SearchFirstName</td><td><input name='SearchFirstName' value='<?PHP echo $post['txtSearchFirstName'];?>'></td>
+  </tr>
+  <tr>
+    <td>SearchLastName</td><td><input name='SearchLastName' value='<?PHP echo $post['txtSearchLastName'];?>'></td>
+  </tr>
+  <tr>
+    <td>DOBMonth</td><td><input name='DOBMonth' value='<?PHP echo $post['txtDOBMonth'];?>'></td>
+  </tr>
+  <tr>
+    <td>DOBDay</td><td><input name='DOBDay' value='<?PHP echo $post['txtDOBDay'];?>'></td>
+  </tr>
+  <tr>
+    <td>DOBYear</td><td><input name='DOBYear' value='<?PHP echo $post['txtDOBYear'];?>'></td>
+  </tr>
+  <tr>
+    <td>SearchZipCode</td><td><input name='SearchZipCode' value='<?PHP echo $post['txtSearchZipCode'];?>'></td>
+  </tr>
+  <tr>
+    <td>SearchHouseNumber*</td><td><input name='SearchHouseNumber' value='<?PHP echo $post['txtSearchHouseNumber'];?>'></td>
+  </tr>
+  <tr>
+    <td>SearchMiddleInitial*</td><td><input name='SearchMiddleInitial' value='<?PHP echo $post['txtSearchMiddleInitial'];?>'></td>
+  </tr>
+  <tr>
+    <td>*Optional</td><td><input type='submit'></td>
+  </tr>
+</table>  
+</form>
+
+
+<?PHP
+function cut_part_out($start,$end,$whole){
+  $parts = explode($start,$whole);
+  $subparts = explode($end,$parts[1]);
+  $out = $subparts[0];
+  return $out;
+}
+
+function getPage($url,$cookie,$post){
+  $url = str_replace('[month]',date('F'),$url); // replace month January through December
+  $url = str_replace('[day]',date('j'),$url); // replace day 1 to 31
+  $url = str_replace('[yesterday]',date('j',strtotime('yesterday')),$url); // replace day 1 to 31
+  $url = str_replace('[year]',date('Y'),$url); // replace year Examples: 1999 or 2003
+  $curl = curl_init();
+  curl_setopt ($curl, CURLOPT_URL, $url);
+  curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire MEPS https://www.md-petition.com/ /%d.0",rand(4,50)));
+  curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt ($curl, CURLOPT_HEADER, 1);
+  curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt ($curl, CURLOPT_FOLLOWLOCATION, true);
+  if ($cookie != ''){
+    curl_setopt ($curl, CURLOPT_HTTPHEADER, array("Cookie: ASP.NET_SessionId=$cookie")); // use cookies 
+  }
+  /* 
+  $postfields = array(
+    'upload_file' => '@file_to_upload.png',
+    'upload_text' => '@text_to_upload'
+  );
+  */
+  if ($post != ''){
+    curl_setopt ($curl, CURLOPT_POSTFIELDS, $post);
+  }
+  $html = curl_exec ($curl);
+  curl_close ($curl);
+  return $html;
+}
+
+// start a session to get a cookie
+$form['url']  = 'https://voterservices.elections.maryland.gov/VoterSearch';
+$form['html'] = getPage($form['url'],'','');
+
+// extract the cookie from the header (see CURLOPT_HEADER)
+$cookie = cut_part_out('ASP.NET_SessionId=',';',$form['html']);
+// extract the form elements we will need to post with our data
+$post['__VIEWSTATE'] = cut_part_out('id="__VIEWSTATE" value="','"',$form['html']);
+$post['__VIEWSTATEGENERATOR'] = cut_part_out('id="__VIEWSTATEGENERATOR" value="','"',$form['html']);
+$post['__VIEWSTATEENCRYPTED'] = cut_part_out('id="__VIEWSTATEENCRYPTED" value="','"',$form['html']);
+$post['__EVENTVALIDATION'] = cut_part_out('id="__EVENTVALIDATION" value="','"',$form['html']);
+// Voter Info Form Elements
+//$post['Languages_field'] = 'ctl00$MainContent$listLanguages';
+
 // debug - show full response make sure we have the cookie
 echo "<li>COOKIE ASP.NET_SessionId $cookie</li>";
 echo "<li>POST VIEWSTATE $post[__VIEWSTATE]</li>";
@@ -130,40 +169,8 @@ echo "</pre>";
 echo "<hr><hr><hr><h1>Voter Form</h1>";
 echo htmlspecialchars($form['html']);
 echo '<hr><hr><hr>';
-//echo $form['html'];
-?>
-<form method='POST'>
-<table>
-  <tr>
-    <td>SearchFirstName</td><td><input name='SearchFirstName'></td>
-  </tr>
-  <tr>
-    <td>SearchLastName</td><td><input name='SearchLastName'></td>
-  </tr>
-  <tr>
-    <td>DOBMonth</td><td><input name='DOBMonth'></td>
-  </tr>
-  <tr>
-    <td>DOBDay</td><td><input name='DOBDay'></td>
-  </tr>
-  <tr>
-    <td>DOBYear</td><td><input name='DOBYear'></td>
-  </tr>
-  <tr>
-    <td>SearchZipCode</td><td><input name='SearchZipCode'></td>
-  </tr>
-  <tr>
-    <td>SearchHouseNumber*</td><td><input name='SearchHouseNumber'></td>
-  </tr>
-  <tr>
-    <td>SearchMiddleInitial*</td><td><input name='SearchMiddleInitial'></td>
-  </tr>
-  <tr>
-    <td>*Optional</td><td><input type='submit'></td>
-  </tr>
-</table>  
-</form>
-<?PHP
+echo $form['html'];
+
 if ($post['txtSearchFirstName'] != ''){
   $result['html'] = getPage($form['url'],$cookie,$post);
   echo "<hr><hr><hr><h1>SBE API RESULTS</h1>";
