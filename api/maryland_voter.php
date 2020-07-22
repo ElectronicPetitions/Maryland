5f36ba049bd676a780c7d5c435a2d0a6dcd00b35
Patrick McGuire <insidenothing@gmail.com>
2020-07-22 10:44:23 -0400

    Update maryland_voter.php

--- api/maryland_voter.php
+++ api/maryland_voter.php
@@ -30,7 +30,7 @@ function MVgetPage($url,$cookie,$post){
   $url = str_replace('[year]',date('Y'),$url); // replace year Examples: 1999 or 2003
   $curl = curl_init();
   curl_setopt ($curl, CURLOPT_URL, $url);
-  curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire MEPS https://www.md-petition.com/ /%d.0",rand(4,50)));
+  //curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire MEPS https://www.md-petition.com/ /%d.0",rand(4,50)));
   curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt ($curl, CURLOPT_HEADER, 1);
   // spoof
