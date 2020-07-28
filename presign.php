b84ce3ecea8b980e9f2d20a31d0446d941067dc1
Patrick McGuire <insidenothing@gmail.com>
2020-07-28 07:31:31 -0400

    Update presign.php

--- presign.php
+++ presign.php
@@ -22,6 +22,7 @@ $signed_name_as             = $petition->real_escape_string($_POST['signed_name_
 $date_of_birth              = $petition->real_escape_string($_COOKIE['pDOB']);
 $signed_name_as_circulator  = $petition->real_escape_string($_POST['signed_name_as_circulator']);
 $contact_phone              = $petition->real_escape_string($_COOKIE['pPHONE']);
+$shared_email               = $petition->real_escape_string($_COOKIE['email']);
 $signature_status           = $petition->real_escape_string($_COOKIE['signature_status']);
 $bot_check                  = $petition->real_escape_string($_SERVER['HTTP_USER_AGENT']);
 $VoterList_table           = $petition->real_escape_string($_COOKIE['VoterList_table']);
@@ -34,8 +35,8 @@ if (empty($_COOKIE['start_time'])){
   $now  = time();
   $time_on_site = $now - $_COOKIE['start_time']; 
 }
-$petition->query("insert into signatures (VoterList_table,php_session_id,bot_check,VTRID,ip_address,date_of_birth,date_time_signed,just_date,petition_id,signed_name_as,signed_name_as_circulator,contact_phone,signature_status)
-values ('$VoterList_table','$php_session_id','$bot_check','$VTRID','$ip','$date_of_birth',NOW(),NOW(),'$petition_id','$signed_name_as','$signed_name_as_circulator','$contact_phone','$signature_status')") or die(mysqli_error($petition));
+$petition->query("insert into signatures (shared_email,VoterList_table,php_session_id,bot_check,VTRID,ip_address,date_of_birth,date_time_signed,just_date,petition_id,signed_name_as,signed_name_as_circulator,contact_phone,signature_status)
+values ('$shared_email','$VoterList_table','$php_session_id','$bot_check','$VTRID','$ip','$date_of_birth',NOW(),NOW(),'$petition_id','$signed_name_as','$signed_name_as_circulator','$contact_phone','$signature_status')") or die(mysqli_error($petition));
 
 $last = $petition->insert_id;
 
