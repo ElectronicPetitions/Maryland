06953565bc6dd6d8c17fd34e2d87addb03667a61
Patrick McGuire <insidenothing@gmail.com>
2020-07-31 09:43:22 -0400

    Update header.php

--- invite/header.php
+++ invite/header.php
@@ -1,2 +1,10 @@
 <?PHP
 include_once('../session.php');
+if (isset($_GET['form_version'])){
+  if ($_GET['form_version'] == '2'){ 
+    setcookie("form_version", '2', time()+3600, "/"); 
+  } 
+  if ($_GET['form_version'] == '3'){ 
+    setcookie("form_version", '3', time()+3600, "/"); 
+  }
+}  
