1696dca624d3f30434924d3238b6780aba9ff9a0
Patrick McGuire <insidenothing@gmail.com>
2021-03-15 16:00:54 -0400

    Update mgp.php

--- invite/mgp.php
+++ invite/mgp.php
@@ -6,14 +6,14 @@ setcookie("invite", "mgp", time()+3600, "/"); // we use this later
 <html lang="en">  
 <head>
   <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165887820-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TY6C66ZWMX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    gtag('config', 'UA-165887820-1');
-  </script>
+  gtag('config', 'G-TY6C66ZWMX');
+</script>
   <?PHP 
   include_once('/var/www/secure.php'); //outside webserver
   presign(); // requires db connection
