0ba2c8f1213784d22573433c5d2d9298e6d95a57
Patrick McGuire <insidenothing@gmail.com>
2022-02-16 16:59:32 -0500

    cleanup

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<style> @media print { .no-print, .no-print * { display: none !important; } } </style>
 <?PHP
 $copy = '&copy; 2020 Patrick McGuire';
 if ($_COOKIE['invite'] != ''){
@@ -5,13 +6,13 @@ if ($_COOKIE['invite'] != ''){
 }  
 ?>
 <?PHP if ($_SERVER['SCRIPT_NAME'] != '/share.php'){ ?>  
- <div class='row'>
+ <div class='row no-print'>
   <div class='col-sm-10' style='text-align:center;'><br><br><br><button type="button" class="btn btn-info btn-lg btn-block" onclick="window.open('share.php')">Before you leave click here to share the petition.</button></div>
  </div>
 <?PHP } ?>
 
 
-<div class='row'>
+<div class='row no-print'>
  <div class='col-sm-10' style='text-align:center;'>
   <?PHP echo $copy;?>
  </div>
@@ -19,7 +20,7 @@ if ($_COOKIE['invite'] != ''){
 
 <br><br>
 
-<div class='row'>
+<div class='row no-print'>
   <div class='col-sm-1' style='text-align:center;'>
   
  </div>
@@ -37,11 +38,7 @@ if ($_COOKIE['invite'] != ''){
 
 
 
-<div class='row'>
- <div class='col-sm-10' style='text-align:center;'>
-  <span id="siteseal"><script async type="text/javascript" src="https://seal.starfieldtech.com/getSeal?sealID=aPXbc3kH1AvnQodUszkMcZir7V0JjcOovILeuCzgYFn2WwPTWyh0o2vI6hDF"></script></span>
- </div>
-</div>
+
 
 <div id="fb-root"></div>
   <script>(function(d, s, id) {
@@ -64,16 +61,5 @@ if ($_COOKIE['invite'] != ''){
 </div>
 
 </div><!-- close container -->
-<script type="text/javascript">
-    window.doorbellOptions = {
-        "id": "10512",
-        "appKey": "4wEnfKuAdj4WlHgKaxLMrAbYn8aKdFhtYVnpbRPkG4AbA7SH7KcgiRvdZyWmwxnw"
-    };
-    (function(w, d, t) {
-        var hasLoaded = false;
-        function l() { if (hasLoaded) { return; } hasLoaded = true; window.doorbellOptions.windowLoaded = true; var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/'+window.doorbellOptions['id']+'?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g); }
-        if (w.attachEvent) { w.attachEvent('onload', l); } else if (w.addEventListener) { w.addEventListener('load', l, false); } else { l(); }
-        if (d.readyState == 'complete') { l(); }
-    }(window, document, 'script'));
-</script>
+
 </body>
