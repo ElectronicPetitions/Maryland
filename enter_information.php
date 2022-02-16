d324e9efa09165814462186a57a8c03f849f665c
Patrick McGuire <insidenothing@gmail.com>
2022-02-16 14:21:02 -0500

    Update enter_information.php

--- enter_information.php
+++ enter_information.php
@@ -32,6 +32,13 @@ if (isset($_POST['web_first_name'])){
      setcookie("web_first_name", $web_first_name);
     }
   }
+  $web_middle_name='';
+  if (isset($_POST['web_middle_name'])){
+    if ($_POST['web_middle_name'] != ''){
+     $web_first_name = $_POST['web_middle_name'];
+     setcookie("web_middle_name", $web_middle_name);
+    }
+  }
   $web_last_name='';
   if (isset($_POST['web_last_name'])){
     if ($_POST['web_last_name'] != ''){
@@ -39,7 +46,9 @@ if (isset($_POST['web_first_name'])){
      setcookie("web_last_name", $web_last_name);
     }
   }
-  if(isset($_POST['web_last_name']) && isset($_POST['web_first_name'])){
+  if(isset($_POST['web_last_name']) && isset($_POST['web_midle_name']) && isset($_POST['web_first_name'])){
+     setcookie("web_name", $web_first_name.' '.$web_middle_name.' '.$web_last_name); 
+  }elseif(isset($_POST['web_last_name']) && isset($_POST['web_first_name'])){
      setcookie("web_name", $web_first_name.' '.$web_last_name); 
   }
   $web_house_number='';
@@ -49,11 +58,32 @@ if (isset($_POST['web_first_name'])){
      setcookie("web_house_number", $web_house_number);
     }
   }
+  $web_street_name='';
+  if (isset($_POST['web_street_name'])){
+    if ($_POST['web_street_name'] != ''){
+     $web_street_name = $_POST['web_street_name'];
+     setcookie("web_street_name", $web_street_name);
+    }
+  }
   $web_zip_code='';
   if (isset($_POST['web_zip_code'])){
     if ($_POST['web_zip_code'] != ''){
      $web_zip_code = $_POST['web_zip_code'];
      setcookie("web_zip_code", $web_zip_code);
+    }
+  }
+  $web_city='';
+  if (isset($_POST['web_city'])){
+    if ($_POST['web_city'] != ''){
+     $web_city = $_POST['web_city'];
+     setcookie("web_city", $web_city);
+    }
+  }
+  $web_county='';
+  if (isset($_POST['web_county'])){
+    if ($_POST['web_county'] != ''){
+     $web_county = $_POST['web_county'];
+     setcookie("web_county", $web_county);
     }
   }
   $contact_phone='';
@@ -92,6 +122,12 @@ slack_general('Enter Information ('.$_COOKIE['invite'].')','md-petition');
   <div class='row'>
     <div class='col-sm-3' style='text-align:right;'><h2>First Name</h2></div>
     <div class='col-sm-6' style='text-align:left;'><input class="form-control input-lg" name='web_first_name' required oninvalid="this.setCustomValidity('Please enter only your first name')" oninput="this.setCustomValidity('')" ></div>
+    <div class='col-sm-1' style='text-align:center;'><button type="submit" class="btn btn-success btn-lg btn-block">↴</button></div>
+  </div>  
+   
+  <div class='row'>
+    <div class='col-sm-3' style='text-align:right;'><h2>Middle Name</h2></div>
+    <div class='col-sm-6' style='text-align:left;'><input class="form-control input-lg" name='web_middle_name' required oninvalid="this.setCustomValidity('Please enter only your middle name')" oninput="this.setCustomValidity('')" ></div>
     <div class='col-sm-1' style='text-align:center;'><button type="submit" class="btn btn-success btn-lg btn-block">↴</button></div>
   </div>  
    
@@ -141,7 +177,22 @@ slack_general('Enter Information ('.$_COOKIE['invite'].')','md-petition');
      <div class='col-sm-3' style='text-align:right; background-color:lightyellow;'><h3>Apartment: 21 Here St. Apt 1323</h3></div>
      <div class='col-sm-6' style='text-align:left; background-color:lightyellow;'><h3>Use: 21</h3></div>
      <div class='col-sm-1' style='text-align:center;'></div>
+  </div> 
+  <div class='row'>
+    <div class='col-sm-3' style='text-align:right;'><h2>Street Name</h2></div>
+    <div class='col-sm-6' style='text-align:left;'><input class="form-control input-lg" name='web_street_name' required oninvalid="this.setCustomValidity('Please enter your street name')" oninput="this.setCustomValidity('')"> </div>
+    <div class='col-sm-1' style='text-align:center;'><button type="submit" class="btn btn-success btn-lg btn-block">↴</button></div>
   </div>  
+  <div class='row'>
+     <div class='col-sm-3' style='text-align:right; background-color:lightyellow;'><h3>House: 321 Here St.</h3></div>
+     <div class='col-sm-6' style='text-align:left; background-color:lightyellow;'><h3>Use: Here St.</h3></div>
+     <div class='col-sm-1' style='text-align:center;'></div>
+  </div>
+  <div class='row'>
+     <div class='col-sm-3' style='text-align:right;'><h2>City</h2></div>
+     <div class='col-sm-6' style='text-align:left;'><input class="form-control input-lg" name='web_city' required oninvalid="this.setCustomValidity('Please enter your city name')" oninput="this.setCustomValidity('')"> </div>
+     <div class='col-sm-1' style='text-align:center;'><button type="submit" class="btn btn-success btn-lg btn-block">↴</button></div>
+  </div>
   <div class='row'>
      <div class='col-sm-3' style='text-align:right;'><h2>ZIP Code</h2></div>
      <div class='col-sm-6' style='text-align:left;'><input class="form-control input-lg" name='web_zip_code' type='number' required oninvalid="this.setCustomValidity('Please enter your five digit zip code')" oninput="this.setCustomValidity('')"> </div>
@@ -151,7 +202,12 @@ slack_general('Enter Information ('.$_COOKIE['invite'].')','md-petition');
      <div class='col-sm-3' style='text-align:right; background-color:lightyellow;'><h3>ZIP Code Format</h3></div>
      <div class='col-sm-6' style='text-align:left; background-color:lightyellow;'><h3>55555</h3></div>
      <div class='col-sm-1' style='text-align:center;'></div>
-  </div> 
+  </div>
+  <div class='row'>
+     <div class='col-sm-3' style='text-align:right;'><h2>County</h2></div>
+     <div class='col-sm-6' style='text-align:left;'><input class="form-control input-lg" name='web_county' required oninvalid="this.setCustomValidity('Please enter your County or Balitmore City')" oninput="this.setCustomValidity('')"> </div>
+     <div class='col-sm-1' style='text-align:center;'><button type="submit" class="btn btn-success btn-lg btn-block">↴</button></div>
+  </div>
   <div class='row'>
      <div class='col-sm-10' style='text-align:center;'><button type="submit" class="btn btn-success btn-lg btn-block"><img alt='Click Here to Continue' class='click_me' src="files/click_here.gif">Next</button></div>
   </div>
@@ -159,10 +215,10 @@ slack_general('Enter Information ('.$_COOKIE['invite'].')','md-petition');
      <div class='col-sm-10' style='text-align:center;'><button type="reset" class="btn btn-warning btn-lg btn-block not_me">Clear</button></div>
   </div>
   <div class='row'>
-     <div class='col-sm-10' style='text-align:center;'>* E-Mail is not required, and will only be used with the petitioners to reach you. It will never be sold.</div>
+     <div class='col-sm-10' style='text-align:center;'>* E-Mail is not required, and will only be used with the petitioners to reach you.</div>
   </div>
      <div class='row'>
-     <div class='col-sm-10' style='text-align:center;'>** PHONE NUMBER IS REQUIRED BY STATE OF MARYLAND. WE WILL NEVER USE YOUR PHONE NUMBER - NEVER CALL - NEVER FOR TEXT MESSAGES.</div>
+     <div class='col-sm-10' style='text-align:center;'>** PHONE NUMBER IS REQUIRED BY STATE OF MARYLAND.</div>
   </div>
 </form>
 
